412990fe895294da484d67eac048208786bbaaee
Patrícia Betíková <patriciabetikova@Patricias-iMac.local>
2022-08-09 20:47:26 +0200

    h37: removed unused import

--- src/components/Typography/GradientText/GradientText.styled.ts
+++ src/components/Typography/GradientText/GradientText.styled.ts
@@ -2,7 +2,6 @@ import { fonts, margins } from "common/styles"
 import styled from "styled-components/macro"
 import { theme } from "theme"
 import { Heading, HeadingProps } from "../Heading/Heading"
-// import { Text, TextProps } from "../Text/Text"
 
 export const StyledGradientText = styled(Heading)<HeadingProps>`
   display: inline-block;
