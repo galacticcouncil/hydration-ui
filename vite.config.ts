94681f0e2569eefa3f850a701b8fd94244a3acb8
David Duong <david@duong.cz>
2022-11-04 09:32:48 +0100

    Fix OOM in Netlify when Sentry is enabled (#148)

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from "vite"
+import { defineConfig, loadEnv, splitVendorChunkPlugin } from "vite"
 import react from "@vitejs/plugin-react"
 import wasm from "vite-plugin-wasm"
 import svgr from "vite-plugin-svgr"
@@ -25,6 +25,7 @@ export default defineConfig(({ mode }) => {
       logOverride: { "this-is-undefined-in-esm": "silent" },
     },
     plugins: [
+      splitVendorChunkPlugin(),
       tsconfigPaths(),
       react({
         jsxImportSource: "@basilisk/jsx",
