d9ca98b6da961a7a4989db39a0fac1d6c5a86307
Duong Tat Dat <david@duong.cz>
2022-11-24 23:30:48 +0100

    Add responsive design

--- src/components/Toast/ToastSidebar.styled.ts
+++ src/components/Toast/ToastSidebar.styled.ts
@@ -2,6 +2,19 @@ import { DialogContent } from "@radix-ui/react-dialog"
 import { theme } from "theme"
 import { IconButton } from "components/IconButton/IconButton"
 import styled from "@emotion/styled"
+import { keyframes } from "@emotion/react"
+
+const slideKeyframe = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateX(100%);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`
 
 export const SWrapper = styled.div`
   position: fixed;
@@ -16,18 +29,40 @@ export const SDialogContent = styled(DialogContent)`
   position: absolute;
   z-index: ${theme.zIndices.modal};
 
-  right: 8px;
-  top: 8px;
-  bottom: 8px;
+  top: unset;
+  left: 0;
+  right: 0;
+  bottom: 0;
 
-  width: calc(100% - 16px);
-  max-width: 400px;
+  width: 100%;
+  max-height: 80vh;
 
   background: ${theme.colors.backgroundGray900};
+  box-shadow: 0px 0px 60px rgba(${theme.rgbColors.black}, 0.36);
+
   border-radius: 16px;
-  box-shadow: 0px 0px 60px rgba(0, 0, 0, 0.36);
+  border-bottom-left-radius: 0px;
+  border-bottom-right-radius: 0px;
+
+  border: 1px solid rgba(${theme.rgbColors.white}, 0.06);
 
   overflow-y: auto;
+
+  @media ${theme.viewport.gte.sm} {
+    left: unset;
+    right: 8px;
+    top: 8px;
+    bottom: 8px;
+
+    animation: 300ms cubic-bezier(0.16, 1, 0.3, 1) ${slideKeyframe};
+
+    width: calc(100% - 16px);
+    max-width: 400px;
+    max-height: unset;
+
+    border-bottom-left-radius: 16px;
+    border-bottom-right-radius: 16px;
+  }
 `
 export const SCloseButton = styled(IconButton)`
   color: ${theme.colors.white};
