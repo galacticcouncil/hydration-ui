e8241410787377a9f70e212cdeabe3a8d0f5497c
Pavol Noha <pavol.noha@gmail.com>
2022-12-06 19:55:15 +0100

    Fix bdf4c49c3f8ab95cb2a8baabd539a658ad284997

--- src/sections/trade/TradePage.tsx
+++ src/sections/trade/TradePage.tsx
@@ -2,11 +2,8 @@ import { Page } from "components/Layout/Page/Page"
 
 import * as React from "react"
 import * as Apps from "@galacticcouncil/apps"
-import { createComponent, EventName } from "@lit-labs/react"
+import { createComponent } from "@lit-labs/react"
 import { useAccountStore } from "state/store"
-import { MakeGenerics, useSearch } from "@tanstack/react-location"
-import { z } from "zod"
-import { Spinner } from "components/Spinner/Spinner.styled"
 
 const NotificationCenter = createComponent({
   tagName: "gc-notification-center",
@@ -24,53 +21,25 @@ export const TradeApp = createComponent({
   tagName: "gc-trade-app",
   elementClass: Apps.TradeApp,
   react: React,
-  events: {
-    onInit: "gc:init" as EventName<CustomEvent>,
-  },
 })
-
-const TradeAppSearch = z.object({
-  type: z.union([z.literal("assetIn"), z.literal("assetOut")]),
-  id: z.number().transform((value) => String(value)),
-})
-
-type SearchGenerics = MakeGenerics<{
-  Search: z.infer<typeof TradeAppSearch>
-}>
 
 export function TradePage() {
   const { account } = useAccountStore()
 
-  const search = useSearch<SearchGenerics>()
-
-  const [loaded, setLoaded] = React.useState(false)
   const ref = React.useRef<Apps.TradeApp>(null)
 
   return (
     <Page>
-      <div sx={{ flex: "column", align: "center" }}>
-        {!loaded && <Spinner width={64} height={64} />}
+      <div>
         <NotificationCenter>
           <TransactionCenter>
             <TradeApp
               ref={ref}
-              css={{ display: loaded ? "block" : "none" }}
               accountName={account?.name}
               accountProvider={account?.provider}
               accountAddress={account?.address}
               apiAddress={import.meta.env.VITE_PROVIDER_URL}
               pools="Omni"
-              onInit={() => {
-                const app = ref.current
-                const safeSearch = TradeAppSearch.safeParse(search)
-
-                if (app != null && safeSearch.success) {
-                  const asset = safeSearch.data
-                  app.setInitialAssets(asset.type, asset.id)
-                }
-
-                setLoaded(true)
-              }}
             />
           </TransactionCenter>
         </NotificationCenter>
