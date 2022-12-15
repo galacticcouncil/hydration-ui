ee99723bb2cb1f6919b7c686498ece4e0bd6f532
vkulinich-cl <115090120+vkulinich-cl@users.noreply.github.com>
2022-12-15 09:38:36 +0100

    Removed license links (#61)

--- src/sections/wallet/connect/providerSelect/WalletConnectProviderSelect.tsx
+++ src/sections/wallet/connect/providerSelect/WalletConnectProviderSelect.tsx
@@ -1,7 +1,6 @@
 import { Text } from "components/Typography/Text/Text"
-import { Trans, useTranslation } from "react-i18next"
-import { Separator } from "components/Separator/Separator"
-import { ExternalLink } from "components/Link/ExternalLink"
+import { useTranslation } from "react-i18next"
+
 import { WalletConnectProviders } from "sections/wallet/connect/providers/WalletConnectProviders"
 import { Wallet } from "@talismn/connect-wallets"
 
@@ -22,23 +21,6 @@ export const WalletConnectProviderSelect = ({ onWalletSelect }: Props) => {
         onConnect={onWalletSelect}
         onDownload={(wallet) => window.open(wallet.installUrl, "_blank")}
       />
-
-      <Text fs={14} fw={400} tAlign="center" color="basic400" sx={{ my: 30 }}>
-        <Trans t={t} i18nKey="walletConnect.provider.terms">
-          <ExternalLink href="/" sx={{ color: "warning100" }} />
-        </Trans>
-      </Text>
-
-      <Separator
-        color="basic800"
-        sx={{ ml: -30, width: "calc(100% + 60px)" }}
-      />
-
-      <Text fw={400} fs={14} tAlign="center" color="basic400" sx={{ mt: 26 }}>
-        <Trans t={t} i18nKey="walletConnect.provider.learn">
-          <ExternalLink href="/" sx={{ color: "brightBlue300" }} />
-        </Trans>
-      </Text>
     </>
   )
 }
