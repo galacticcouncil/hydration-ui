galacticcouncil/Basilisk-ui

added 24h volume

src/api/volume.ts
@@ -2,12 +2,12 @@ import { u8aToHex } from "@polkadot/util"
 import { decodeAddress } from "@polkadot/util-crypto"
 import { useQuery } from "@tanstack/react-query"
 import { addDays } from "date-fns"
-import { request, gql } from "graphql-request"
+import { gql, request } from "graphql-request"
 import { Maybe, undefinedNoop } from "utils/helpers"
 import { QUERY_KEYS } from "utils/queryKeys"
 
-const getTradeVolume = (poolAddress: string) => async () => {
-  const poolHex = u8aToHex(decodeAddress(poolAddress))
+const getTradeVolume = (assetId: string) => async () => {
+  const assetHex = u8aToHex(decodeAddress(assetId))
   const after = addDays(new Date(), -1).toISOString()
 
   // This is being typed manually, as GraphQL schema does not
@@ -15,43 +15,37 @@ const getTradeVolume = (poolAddress: string) => async () => {
   return await request<{
     events: Array<
       | {
-          name: "XYK.SellExecuted"
+          name: "Omnipool.SellExecuted"
           args: {
             who: string
-            assetOut: number
             assetIn: number
-            amount: string
-            salePrice: string
-            feeAsset: number
-            feeAmount: string
-            pool: string
+            assetOut: number
+            amount_in: string
+            amount_out: string
           }
         }
       | {
-          name: "XYK.BuyExecuted"
+          name: "Omnipool.BuyExecuted"
           args: {
             who: string
-            assetOut: number
             assetIn: number
-            amount: string
-            buyPrice: string
-            feeAsset: number
-            feeAmount: string
-            pool: string
+            assetOut: number
+            amount_in: string
+            amount_out: string
           }
         }
     >
   }>(
     import.meta.env.VITE_INDEXER_URL,
     gql`
-      query TradeVolume($poolHex: String!, $after: DateTime!) {
+      query TradeVolume($assetHex: String!, $after: DateTime!) {
         events(
           where: {
-            args_jsonContains: { pool: $poolHex }
+            args_jsonContains: { assetIn: $assetHex }
             block: { timestamp_gte: $after }
             AND: {
-              name_eq: "XYK.SellExecuted"
-              OR: { name_eq: "XYK.BuyExecuted" }
+              name_eq: "Omnipool.SellExecuted"
+              OR: { name_eq: "Omnipool.BuyExecuted" }
             }
           }
         ) {
@@ -63,14 +57,14 @@ const getTradeVolume = (poolAddress: string) => async () => {
         }
       }
     `,
-    { poolHex, after },
+    { assetHex, after },
   )
 }
 
-export function useTradeVolume(poolAddress: Maybe<string>) {
+export function useTradeVolume(assetId: Maybe<string>) {
   return useQuery(
-    QUERY_KEYS.tradeVolume(poolAddress),
-    poolAddress != null ? getTradeVolume(poolAddress) : undefinedNoop,
-    { enabled: !!poolAddress },
+    QUERY_KEYS.tradeVolume(assetId),
+    assetId != null ? getTradeVolume(assetId) : undefinedNoop,
+    { enabled: !!assetId },
   )
 }

galacticcouncil/hydration-ui

src/api/volume.ts
@@ -1,57 +1,52 @@
-import { u8aToHex } from "@polkadot/util"
-import { decodeAddress } from "@polkadot/util-crypto"
 import { useQuery } from "@tanstack/react-query"
 import { addDays } from "date-fns"
-import { request, gql } from "graphql-request"
+import { gql, request } from "graphql-request"
 import { Maybe, undefinedNoop } from "utils/helpers"
 import { QUERY_KEYS } from "utils/queryKeys"
+import { u32 } from "@polkadot/types-codec"
 
-const getTradeVolume = (poolAddress: string) => async () => {
-  const poolHex = u8aToHex(decodeAddress(poolAddress))
+export const getTradeVolume = (assetId: u32) => async () => {
+  const assetIn = assetId.toNumber()
   const after = addDays(new Date(), -1).toISOString()
+
+  console.log(assetIn)
 
   // This is being typed manually, as GraphQL schema does not
   // describe the event arguments at all
   return await request<{
     events: Array<
       | {
-          name: "XYK.SellExecuted"
+          name: "Omnipool.SellExecuted"
           args: {
             who: string
-            assetOut: number
             assetIn: number
-            amount: string
-            salePrice: string
-            feeAsset: number
-            feeAmount: string
-            pool: string
+            assetOut: number
+            amountIn: string
+            amountOut: string
           }
         }
       | {
-          name: "XYK.BuyExecuted"
+          name: "Omnipool.BuyExecuted"
           args: {
             who: string
-            assetOut: number
             assetIn: number
-            amount: string
-            buyPrice: string
-            feeAsset: number
-            feeAmount: string
-            pool: string
+            assetOut: number
+            amountIn: string
+            amountOut: string
           }
         }
     >
   }>(
     import.meta.env.VITE_INDEXER_URL,
     gql`
-      query TradeVolume($poolHex: String!, $after: DateTime!) {
+      query TradeVolume($assetIn: Int!, $after: DateTime!) {
         events(
           where: {
-            args_jsonContains: { pool: $poolHex }
+            args_jsonContains: { asset_in: $assetIn }
             block: { timestamp_gte: $after }
             AND: {
-              name_eq: "XYK.SellExecuted"
-              OR: { name_eq: "XYK.BuyExecuted" }
+              name_eq: "Omnipool.SellExecuted"
+              OR: { name_eq: "Omnipool.BuyExecuted" }
             }
           }
         ) {
@@ -63,14 +58,14 @@ const getTradeVolume = (poolAddress: string) => async () => {
         }
       }
     `,
-    { poolHex, after },
+    { assetIn, after },
   )
 }
 
-export function useTradeVolume(poolAddress: Maybe<string>) {
+export function useTradeVolume(assetId: Maybe<u32>) {
   return useQuery(
-    QUERY_KEYS.tradeVolume(poolAddress),
-    poolAddress != null ? getTradeVolume(poolAddress) : undefinedNoop,
-    { enabled: !!poolAddress },
+    QUERY_KEYS.tradeVolume(assetId),
+    assetId != null ? getTradeVolume(assetId) : undefinedNoop,
+    { enabled: !!assetId },
   )
 }