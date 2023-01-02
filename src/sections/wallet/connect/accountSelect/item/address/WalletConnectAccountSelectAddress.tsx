9812268f479f4dd9f39fc2ff60becbfffd6bfd02
vkulinich-cl <115090120+vkulinich-cl@users.noreply.github.com>
2023-01-02 10:17:37 +0100

    Polish Account Select (#84)
    * Polish Account Select
    
    * Revert clipboard changes
    
    * Revert "Revert clipboard changes"
    
    This reverts commit 55773639357d2ee05f7e17da41f93cd806d80544.
    
    * Show copy icon for selected account

--- src/sections/wallet/connect/accountSelect/item/address/WalletConnectAccountSelectAddress.tsx
+++ src/sections/wallet/connect/accountSelect/item/address/WalletConnectAccountSelectAddress.tsx
@@ -3,16 +3,17 @@ import { Text } from "components/Typography/Text/Text"
 import { FC } from "react"
 import { AccountAvatar } from "components/AccountAvatar/AccountAvatar"
 import { shortenAccountAddress } from "utils/formatting"
-import { ButtonTransparent } from "components/Button/Button"
 import { InfoTooltip } from "components/InfoTooltip/InfoTooltip"
 import { useCopyToClipboard } from "react-use"
 import { useTranslation } from "react-i18next"
+import { ReactComponent as CopyIcon } from "assets/icons/CopyIcon.svg"
 
 type Props = {
   name: string
   theme: string
   address: string
   onClick?: () => void
+  isActive: boolean
 }
 
 export const WalletConnectAccountSelectAddress: FC<Props> = ({
@@ -20,6 +21,7 @@ export const WalletConnectAccountSelectAddress: FC<Props> = ({
   theme,
   address,
   onClick,
+  isActive,
 }) => {
   const { t } = useTranslation()
   const [, copy] = useCopyToClipboard()
@@ -27,40 +29,46 @@ export const WalletConnectAccountSelectAddress: FC<Props> = ({
   return (
     <div
       onClick={onClick}
-      sx={{ flex: "row", align: "center", gap: 10 }}
+      sx={{ flex: "row", align: "center", gap: 10, justify: "space-between" }}
       css={{ position: "relative" }}
     >
-      <div
-        sx={{ p: 5, flex: "row", align: "center" }}
-        css={{ borderRadius: "9999px" }}
-      >
+      <div sx={{ flex: "row" }}>
+        <div
+          sx={{ p: 5, flex: "row", align: "center" }}
+          css={{ borderRadius: "9999px" }}
+        >
+          <AccountAvatar address={address} theme={theme} size={32} />
+        </div>
+
+        <div sx={{ flex: "column", gap: 3 }} css={{ overflow: "hidden" }}>
+          <Text fw={600} fs={12}>
+            {name}
+          </Text>
+          <Text
+            fw={600}
+            fs={14}
+            css={css`
+              overflow: hidden;
+              text-overflow: ellipsis;
+              white-space: nowrap;
+              color: var(--secondary-color);
+            `}
+          >
+            {shortenAccountAddress(address, 12)}
+          </Text>
+        </div>
+      </div>
+      {isActive && (
         <InfoTooltip
           text={t("wallet.header.copyAddress.hover")}
           textOnClick={t("wallet.header.copyAddress.click")}
         >
-          <ButtonTransparent onClick={() => copy(address.toString())}>
-            <AccountAvatar address={address} theme={theme} size={32} />
-          </ButtonTransparent>
+          <CopyIcon
+            css={{ cursor: "pointer", color: "var(--secondary-color)" }}
+            onClick={() => copy(address.toString())}
+          />
         </InfoTooltip>
-      </div>
-
-      <div sx={{ flex: "column", gap: 3 }} css={{ overflow: "hidden" }}>
-        <Text fw={600} fs={12}>
-          {name}
-        </Text>
-        <Text
-          fw={600}
-          fs={14}
-          css={css`
-            overflow: hidden;
-            text-overflow: ellipsis;
-            white-space: nowrap;
-            color: var(--secondary-color);
-          `}
-        >
-          {shortenAccountAddress(address, 12)}
-        </Text>
-      </div>
+      )}
     </div>
   )
 }
