e939ccd6fa00407678cf27d086feda613ec6586f
obsivac <filip.obsivac@cleevio.com>
2023-01-12 13:47:24 +0100

    updated asset tradability conditions

--- src/api/omnipool.ts
+++ src/api/omnipool.ts
@@ -7,20 +7,25 @@ import { ITuple } from "@polkadot/types-codec/types"
 
 export const useOmnipoolAsset = (id: u32 | string) => {
   const api = useApiPromise()
-
   return useQuery(QUERY_KEYS.omnipoolAsset(id), getOmnipoolAsset(api, id))
 }
 
 export const useOmnipoolAssets = () => {
   const api = useApiPromise()
-
   return useQuery(QUERY_KEYS.omnipoolAssets, getOmnipoolAssets(api))
 }
+
+export const useHubAssetTradability = () => {
+  const api = useApiPromise()
+  return useQuery(QUERY_KEYS.hubAssetTradability, getHubAssetTradability(api))
+}
+
+export const getHubAssetTradability = (api: ApiPromise) => async () =>
+  api.query.omnipool.hubAssetTradability()
 
 export const getOmnipoolAsset =
   (api: ApiPromise, id: u32 | string) => async () => {
     const res = await api.query.omnipool.assets(id)
-
     return res.unwrap()
   }
 
@@ -49,7 +54,6 @@ export const useOmnipoolPositions = (itemIds: u128[]) => {
 
 export const useOmnipoolFee = () => {
   const api = useApiPromise()
-
   return useQuery(QUERY_KEYS.omnipoolFee, getOmnipoolFee(api))
 }
 
