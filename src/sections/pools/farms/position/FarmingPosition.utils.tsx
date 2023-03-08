ec5884f0dd21eb7a617a373d0e369276ac353ec8
vkulinich-cl <viacheslav.kulinich@cleevio.com>
2023-03-08 11:10:28 +0100

    Use QueryReduce

--- src/sections/pools/farms/position/FarmingPosition.utils.tsx
+++ src/sections/pools/farms/position/FarmingPosition.utils.tsx
@@ -12,7 +12,7 @@ import { useSpotPrice } from "api/spotPrice"
 import { OMNIPOOL_ACCOUNT_ADDRESS } from "utils/api"
 import { BN_10, BN_NAN } from "utils/constants"
 import BN from "bignumber.js"
-import { useMemo } from "react"
+import { useQueryReduce } from "utils/helpers"
 
 export const useDepositShare = (poolId: u32, depositNftId: string) => {
   const apiIds = useApiIds()
@@ -28,92 +28,87 @@ export const useDepositShare = (poolId: u32, depositNftId: string) => {
   const valueSp = useSpotPrice(position.data?.assetId, apiIds.data?.usdId)
   const lrnaSp = useSpotPrice(apiIds.data?.hubId, apiIds.data?.usdId)
 
-  const queries = [
-    apiIds,
-    omnipoolBalance,
-    omnipoolAsset,
-    meta,
-    lrnaMeta,
-    positionId,
-    position,
-    valueSp,
-    lrnaSp,
-  ]
-  const isLoading = queries.some((q) => q.isInitialLoading)
-
-  const data = useMemo(() => {
-    if (
-      !(
-        omnipoolBalance.data &&
-        omnipoolAsset.data &&
-        position.data &&
-        lrnaMeta.data &&
-        meta.data &&
-        valueSp.data &&
-        lrnaSp.data
+  return useQueryReduce(
+    [
+      omnipoolBalance,
+      omnipoolAsset,
+      meta,
+      lrnaMeta,
+      position,
+      valueSp,
+      lrnaSp,
+    ] as const,
+    (
+      omnipoolBalance,
+      omnipoolAsset,
+      meta,
+      lrnaMeta,
+      position,
+      valueSp,
+      lrnaSp,
+    ) => {
+      if (
+        !(
+          omnipoolBalance &&
+          omnipoolAsset &&
+          position &&
+          lrnaMeta &&
+          meta &&
+          valueSp &&
+          lrnaSp
+        )
       )
-    )
-      return null
+        return null
 
-    let lernaOutResult = "-1"
-    let liquidityOutResult = "-1"
+      let lernaOutResult = "-1"
+      let liquidityOutResult = "-1"
 
-    const [nom, denom] =
-      position.data.price.map((n) => new BN(n.toString())) ?? []
-    const price = nom.div(denom)
-    const positionPrice = price.times(BN_10.pow(18))
+      const [nom, denom] = position.price.map((n) => new BN(n.toString())) ?? []
+      const price = nom.div(denom)
+      const positionPrice = price.times(BN_10.pow(18))
 
-    const params: Parameters<typeof calculate_liquidity_out> = [
-      omnipoolBalance.data.balance.toString(),
-      omnipoolAsset.data.hubReserve.toString(),
-      omnipoolAsset.data.shares.toString(),
-      position.data.amount.toString(),
-      position.data.shares.toString(),
-      positionPrice.toFixed(0),
-      position.data.shares.toString(),
-    ]
+      const params: Parameters<typeof calculate_liquidity_out> = [
+        omnipoolBalance.balance.toString(),
+        omnipoolAsset.hubReserve.toString(),
+        omnipoolAsset.shares.toString(),
+        position.amount.toString(),
+        position.shares.toString(),
+        positionPrice.toFixed(0),
+        position.shares.toString(),
+      ]
 
-    lernaOutResult = calculate_liquidity_lrna_out.apply(this, params)
-    liquidityOutResult = calculate_liquidity_out.apply(this, params)
+      lernaOutResult = calculate_liquidity_lrna_out.apply(this, params)
+      liquidityOutResult = calculate_liquidity_out.apply(this, params)
 
-    const lrnaDp = BN_10.pow(lrnaMeta.data?.decimals.toNumber() ?? 12)
-    const lrna =
-      lernaOutResult !== "-1" ? new BN(lernaOutResult).div(lrnaDp) : BN_NAN
+      const lrnaDp = BN_10.pow(lrnaMeta.decimals.toNumber() ?? 12)
+      const lrna =
+        lernaOutResult !== "-1" ? new BN(lernaOutResult).div(lrnaDp) : BN_NAN
 
-    const valueDp = BN_10.pow(meta.data?.decimals.toNumber() ?? 12)
-    const value =
-      liquidityOutResult !== "-1"
-        ? new BN(liquidityOutResult).div(valueDp)
-        : BN_NAN
+      const valueDp = BN_10.pow(meta.decimals.toNumber() ?? 12)
+      const value =
+        liquidityOutResult !== "-1"
+          ? new BN(liquidityOutResult).div(valueDp)
+          : BN_NAN
 
-    let valueUSD = BN_NAN
+      let valueUSD = BN_NAN
 
-    if (liquidityOutResult !== "-1" && valueSp?.data) {
-      valueUSD = value.times(valueSp.data.spotPrice)
+      if (liquidityOutResult !== "-1" && valueSp) {
+        valueUSD = value.times(valueSp.spotPrice)
 
-      if (lrna.gt(0)) {
-        valueUSD = !lrnaSp?.data
-          ? BN_NAN
-          : valueUSD.plus(lrna.times(lrnaSp.data.spotPrice))
+        if (lrna.gt(0)) {
+          valueUSD = !lrnaSp
+            ? BN_NAN
+            : valueUSD.plus(lrna.times(lrnaSp.spotPrice))
+        }
       }
-    }
 
-    return {
-      ...position.data,
-      value,
-      valueUSD,
-      lrna,
-      symbol: meta.data?.symbol,
-    }
-  }, [
-    position.data,
-    omnipoolBalance.data,
-    omnipoolAsset.data,
-    lrnaMeta.data,
-    meta.data,
-    valueSp.data,
-    lrnaSp.data,
-  ])
-
-  return { data, isLoading }
+      return {
+        ...position,
+        value,
+        valueUSD,
+        lrna,
+        symbol: meta.symbol,
+      }
+    },
+  )
 }
