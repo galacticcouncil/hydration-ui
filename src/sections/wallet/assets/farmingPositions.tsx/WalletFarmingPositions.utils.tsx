93bfe00028783cfa0e3af179d1bb7e79f53c5cf0
obsivac-cleevio <filip.obsivac@cleevio.com>
2023-03-22 13:59:24 +0100

    Revert "updated type"
    This reverts commit c6f5309625acf6513c9340d2bcf2441a8c142951.

--- src/sections/wallet/assets/farmingPositions.tsx/WalletFarmingPositions.utils.tsx
+++ src/sections/wallet/assets/farmingPositions.tsx/WalletFarmingPositions.utils.tsx
@@ -10,6 +10,7 @@ import { useAssetDetailsList } from "api/assetDetails"
 import { useAssetMetaList } from "api/assetMeta"
 import { useBestNumber } from "api/chain"
 import { useAccountDepositIds, useAllDeposits } from "api/deposits"
+import BN from "bignumber.js"
 import { getAssetName } from "components/AssetIcon/AssetIcon"
 import { DollarAssetValue } from "components/DollarAssetValue/DollarAssetValue"
 import { Text } from "components/Typography/Text/Text"
@@ -153,7 +154,7 @@ export const useFarmingPositionsData = () => {
     )
       return []
 
-    const rows = accountDeposits.map((deposit) => {
+    const rows: FarmingPositionsTableData[] = accountDeposits.map((deposit) => {
       const id = deposit.id.toString()
       const assetId = deposit.deposit.ammPoolId.toString()
       const meta = assetMetas.data.find((am) => am.id === assetId)
@@ -195,6 +196,16 @@ export const useFarmingPositionsData = () => {
   return { data, isLoading }
 }
 
-export type FarmingPositionsTableData = NonNullable<
-  ReturnType<typeof useFarmingPositionsData>["data"]
->[number]
+export type FarmingPositionsTableData = {
+  id: string
+  symbol: string
+  name: string
+  date: Date
+  shares: BN
+  position: {
+    symbol: string
+    value: BN
+    valueUSD: BN
+    lrna: BN
+  }
+}
