e8cbfcdd206cfc903036d7287864bbc37ca30e14
vkulinich <viacheslav.kulinich@cleevio.com>
2023-05-10 16:55:39 +0200

    Rework Withdrawal fee

--- src/sections/pools/modals/RemoveLiquidity/components/FeeRange/FeeRange.tsx
+++ src/sections/pools/modals/RemoveLiquidity/components/FeeRange/FeeRange.tsx
@@ -19,7 +19,6 @@ import { useMemo, useState } from "react"
 import { SummaryRow } from "components/Summary/SummaryRow"
 import { AnimatePresence, motion } from "framer-motion"
 import Skeleton from "react-loading-skeleton"
-import { SSeparator } from "components/Separator/Separator.styled"
 
 const FEE_RANGE_COLOR_CONFIG: Record<number, keyof typeof theme.colors> = {
   0: "green600",
@@ -117,26 +116,24 @@ export const FeeRange = ({
             <SFullRangeContainer>
               <div sx={{ flex: "row", justify: "space-between" }}>
                 <Text fs={13} color="basic100">
-                  {t("liquidity.remove.modal.feeRange.tokenFee.label", {
+                  {t("liquidity.remove.modal.feeRange.label")}
+                </Text>
+                <Text color="green500" fs={13}>
+                  {t("value.tokenWithSymbol", {
+                    value: assetFeeValue,
                     symbol: assetSymbol,
                   })}
                 </Text>
-                <Text color="green500" fs={13}>
-                  {t("value.token", { value: assetFeeValue })}
-                </Text>
               </div>
               {lrnaFeeValue && (
-                <>
-                  <SSeparator />
-                  <div sx={{ flex: "row", justify: "space-between" }}>
-                    <Text fs={13} color="basic100">
-                      {t("liquidity.remove.modal.feeRange.lrnaFee.label")}
-                    </Text>
-                    <Text color="green500" fs={13}>
-                      {t("value.token", { value: lrnaFeeValue })}
-                    </Text>
-                  </div>
-                </>
+                <div sx={{ flex: "row", justify: "end" }}>
+                  <Text color="green500" fs={13}>
+                    {t("value.tokenWithSymbol", {
+                      value: lrnaFeeValue,
+                      symbol: "LRNA",
+                    })}
+                  </Text>
+                </div>
               )}
 
               <div sx={{ mt: 8 }} css={{ position: "relative" }}>
