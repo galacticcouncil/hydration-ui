451e78feafd1f1594cb3f5ccb70edec60e71027b
Jakub Kolebaba <jakub@kolebaba.cz>
2023-07-12 14:11:33 +0200

    Stat page burn section

---
+++ src/sections/stats/sections/LRNA/sections/Burn.tsx
@@ -0,0 +1,36 @@
+import { PieChart } from '../components/PieChart/PieChart'
+import { ReactComponent as StakingRewardIcon } from "assets/icons/StakingRewardIcon.svg"
+import { ReactComponent as StakedAPRIcon } from "assets/icons/StakedAPR.svg"
+import { Icon } from 'components/Icon/Icon'
+import { Text } from 'components/Typography/Text/Text'
+import { SBurnContainer } from './Burn.styled'
+
+export const Burn = () => {
+  return (
+    <SBurnContainer>
+      <div>
+        <PieChart percentage={20} loading={false} />
+      </div>
+      <div>
+        <Icon
+          size={18}
+          sx={{ color: "brightBlue300", m: 'auto' }}
+          icon={<StakingRewardIcon />}
+        />
+        <Text color="brightBlue300" sx={{ my: '10px' }}>To be burned:</Text>
+        <Text fs={[20, 30]} lh={[20, 30]} font="FontOver">2 455 LRNA</Text>
+        <Text color="darkBlue200" fs={14}>≈$59509,2</Text>
+      </div>
+      <div>
+        <Icon
+          size={18}
+          sx={{ color: "brightBlue300", m: 'auto' }}
+          icon={<StakedAPRIcon />}
+        />
+        <Text color="brightBlue300" sx={{ my: '10px' }}>Protocol fees last 24h</Text>
+        <Text fs={[20, 30]} lh={[20, 30]} font="FontOver">1 455 LRNA</Text>
+        <Text color="darkBlue200" fs={14}>≈$24.24</Text>
+      </div>
+    </SBurnContainer>
+  )
+}
