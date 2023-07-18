b2f8f7bd48baa7d3f23c8484fe0002b423581e7f
Jakub Kolebaba <jakub@kolebaba.cz>
2023-07-18 08:14:12 +0200

    Updated translations

--- src/sections/stats/sections/LRNA/components/PieChart/PieChartLabel.tsx
+++ src/sections/stats/sections/LRNA/components/PieChart/PieChartLabel.tsx
@@ -1,11 +1,14 @@
 import { Text } from "components/Typography/Text/Text"
+import { useTranslation } from 'react-i18next'
 
 export const PieChartLabel = () => {
+  const { t } = useTranslation();
+
   return (
     <>
       <Text fs={12}>status:</Text>
       <Text fs={20} font="FontOver">
-        BURNING
+        {t('stats.lrna.status.burning')}
       </Text>
     </>
   )
