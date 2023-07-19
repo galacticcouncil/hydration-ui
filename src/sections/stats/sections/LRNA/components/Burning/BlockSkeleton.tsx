fa5036697990863cdac5f88ee6c6cd13de9b6abb
Jakub Kolebaba <jakub@kolebaba.cz>
2023-07-19 11:54:05 +0200

    Lint fix

--- src/sections/stats/sections/LRNA/components/Burning/BlockSkeleton.tsx
+++ src/sections/stats/sections/LRNA/components/Burning/BlockSkeleton.tsx
@@ -2,7 +2,7 @@ import Skeleton from "react-loading-skeleton"
 
 export const BlockSkeleton = () => {
   return (
-    <div sx={{ textAlign: 'center' }}>
+    <div sx={{ textAlign: "center" }}>
       <Skeleton width={200} height={30} />
       <Skeleton width={100} height={10} />
     </div>
