63cbaa4acc322a2915f5391c1d1d368a0229ca8f
Jakub Kolebaba <jakub@kolebaba.cz>
2023-08-18 10:18:35 +0200

    Add translations

--- src/sections/pools/stablepool/transfer/TransferOptions.tsx
+++ src/sections/pools/stablepool/transfer/TransferOptions.tsx
@@ -2,6 +2,7 @@ import { ReactComponent as WaterRippleIcon } from "assets/icons/WaterRippleIcon.
 import { ReactComponent as DropletIcon } from "assets/icons/DropletIcon.svg"
 import { TransferOption } from "./TransferOption"
 import { ListItem } from "./ListItem"
+import { useTranslation } from "react-i18next"
 
 type Option = "OMNIPOOL" | "STABLEPOOL"
 
@@ -10,43 +11,46 @@ type Props = {
   onSelect: (selected: Option) => void
 }
 
-export const TransferOptions = ({ selected, onSelect }: Props) => (
-  <>
-    <TransferOption
-      selected={selected === "OMNIPOOL"}
-      onSelect={() => onSelect("OMNIPOOL")}
-      heading="Add to omnipool"
-      subheading="Benefits"
-      icon={<WaterRippleIcon />}
-    >
-      <ListItem>
-        First benefit mentioned here. A line of of text would be enough.
-      </ListItem>
-      <ListItem>
-        Second benefit mentioned here. A line of of text would be enough.
-      </ListItem>
-      <ListItem>
-        Third benefit mentioned here. A line of of text would be enough.
-      </ListItem>
-      <ListItem>Fourth benefit mentioned here.</ListItem>
-    </TransferOption>
-    <TransferOption
-      selected={selected === "STABLEPOOL"}
-      onSelect={() => onSelect("STABLEPOOL")}
-      heading="Add to stablepool"
-      subheading="Benefits"
-      icon={<DropletIcon />}
-    >
-      <ListItem>
-        First benefit mentioned here. A line of of text would be enough.
-      </ListItem>
-      <ListItem>
-        Second benefit mentioned here. A line of of text would be enough.
-      </ListItem>
-      <ListItem>
-        Third benefit mentioned here. A line of of text would be enough.
-      </ListItem>
-      <ListItem>Fourth benefit mentioned here.</ListItem>
-    </TransferOption>
-  </>
-)
+export const TransferOptions = ({ selected, onSelect }: Props) => {
+  const { t } = useTranslation()
+  return (
+    <>
+      <TransferOption
+        selected={selected === "OMNIPOOL"}
+        onSelect={() => onSelect("OMNIPOOL")}
+        heading={t("liquidity.add.omnipool")}
+        subheading={t("liquidity.add.benefits")}
+        icon={<WaterRippleIcon />}
+      >
+        <ListItem>
+          First benefit mentioned here. A line of of text would be enough.
+        </ListItem>
+        <ListItem>
+          Second benefit mentioned here. A line of of text would be enough.
+        </ListItem>
+        <ListItem>
+          Third benefit mentioned here. A line of of text would be enough.
+        </ListItem>
+        <ListItem>Fourth benefit mentioned here.</ListItem>
+      </TransferOption>
+      <TransferOption
+        selected={selected === "STABLEPOOL"}
+        onSelect={() => onSelect("STABLEPOOL")}
+        heading={t("liquidity.add.stablepool")}
+        subheading={t("liquidity.add.benefits")}
+        icon={<DropletIcon />}
+      >
+        <ListItem>
+          First benefit mentioned here. A line of of text would be enough.
+        </ListItem>
+        <ListItem>
+          Second benefit mentioned here. A line of of text would be enough.
+        </ListItem>
+        <ListItem>
+          Third benefit mentioned here. A line of of text would be enough.
+        </ListItem>
+        <ListItem>Fourth benefit mentioned here.</ListItem>
+      </TransferOption>
+    </>
+  )
+}
