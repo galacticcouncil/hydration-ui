6d70475c011532218c342ac190d93d2b812facec
Jakub Kolebaba <jakub@kolebaba.cz>
2023-08-26 11:05:51 +0200

    Why bonds section

--- src/sections/trade/sections/bonds/BondsPage.tsx
+++ src/sections/trade/sections/bonds/BondsPage.tsx
@@ -1,8 +1,13 @@
 import { Heading } from "components/Typography/Heading/Heading"
 import { Bond } from "components/Bond/Bond"
 import { AssetLogo } from "components/AssetIcon/AssetIcon"
-import { gradientBorder } from "theme"
+import { gradientBorder, theme } from "theme"
 import { Text } from "components/Typography/Text/Text"
+import { ReactComponent as ClockIcon } from "assets/icons/ClockIcon.svg"
+import { ReactComponent as TickIcon } from "assets/icons/TickIcon.svg"
+import { ReactComponent as DollarIcon } from "assets/icons/Dollar2Icon.svg"
+import { Step } from "./components/Step"
+import { ReactComponent as LinkIcon } from "assets/icons/LinkIcon.svg"
 
 export const BondsPage = () => {
   return (
@@ -38,6 +43,45 @@ export const BondsPage = () => {
         <Text color="white" fs={20} fw={600}>
           Why use Bonds?
         </Text>
+        <Text color="darkBlue200" sx={{ mt: 16 }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec
+          commodo libero. Nulla facilisi. Donec venenatis fringilla elit, ac
+          feugiat sem volutpat id. Nam consectetur viverra feugiat.Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit. Quisque nec commodo
+          libero. Nulla facilisi.
+        </Text>
+        <div sx={{ mt: 41, flex: "row", gap: 20 }}>
+          <Step
+            icon={<TickIcon sx={{ color: "vibrantBlue200" }} />}
+            title="Buy bonded assets with discount"
+            description="Buy bonds at a discounted rate, giving you an instant edge."
+          />
+          <Step
+            icon={<ClockIcon sx={{ color: "brightBlue300" }} />}
+            title="Wait for Maturity"
+            description="Bond needs to reach its maturity before you can redeem it.Maturity is specified at bond creation"
+          />
+          <Step
+            icon={<DollarIcon sx={{ color: "green600" }} />}
+            title="Profit"
+            description="As your bonds reach maturity date, you can redeem them for underlying asset 1:1."
+          />
+        </div>
+        <div>
+          <Text
+            color="brightBlue300"
+            fs={12}
+            css={{
+              borderBottom: `1px solid ${theme.colors.brightBlue300}`,
+              display: "inline-block",
+            }}
+          >
+            <a href="https://hydradx.io" target="_blank" rel="noreferrer">
+              Want to learn more? Check Documentation
+              <LinkIcon height={10} sx={{ ml: 6 }} />
+            </a>
+          </Text>
+        </div>
       </div>
     </>
   )
