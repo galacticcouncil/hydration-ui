6fd500ad7633254199684dbda52f81de61f63bb7
vkulinich <viacheslav.kulinich@cleevio.com>
2023-09-13 20:26:47 +0200

    Enable staking page

--- src/components/Layout/Header/menu/HeaderMenu.tsx
+++ src/components/Layout/Header/menu/HeaderMenu.tsx
@@ -3,14 +3,38 @@ import { SItem, SList } from "components/Layout/Header/menu/HeaderMenu.styled"
 import { useTranslation } from "react-i18next"
 import { MENU_ITEMS } from "utils/navigation"
 import { HeaderSubMenu } from "./HeaderSubMenu"
+import { useProviderRpcUrlStore } from "api/provider"
+import { useBestNumber } from "api/chain"
+import { useApiPromise } from "utils/api"
+import { isApiLoaded } from "utils/helpers"
+import { enableStakingBlock } from "utils/constants"
 
 export const HeaderMenu = () => {
   const { t } = useTranslation()
   const { account } = useSearch()
+  const providers = useProviderRpcUrlStore()
+  const api = useApiPromise()
+  const bestNumber = useBestNumber(!isApiLoaded(api))
+
+  const isMainnet =
+    (providers.rpcUrl ?? import.meta.env.VITE_PROVIDER_URL) ===
+      "wss://rpc.hydradx.cloud" || import.meta.env.VITE_ENV === "production"
+
+  const blockNumber = bestNumber.data?.parachainBlockNumber.toNumber()
+
+  if (!blockNumber) return null
 
   return (
     <SList>
       {MENU_ITEMS.map((item, i) => {
+        if (
+          item.key === "staking" &&
+          isMainnet &&
+          blockNumber < enableStakingBlock
+        ) {
+          return null
+        }
+
         if (!item.enabled) {
           return null
         }
