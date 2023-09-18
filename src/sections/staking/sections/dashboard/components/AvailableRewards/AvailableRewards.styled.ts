b65cd79448d1bb0748b6b6a947fc34922446c376
vkulinich <viacheslav.kulinich@cleevio.com>
2023-09-18 19:24:13 +0200

    wip

--- src/sections/staking/sections/dashboard/components/AvailableRewards/AvailableRewards.styled.ts
+++ src/sections/staking/sections/dashboard/components/AvailableRewards/AvailableRewards.styled.ts
@@ -3,14 +3,6 @@ import { SContainer } from "sections/staking/StakingPage.styled"
 
 export const SRewardCardContainer = styled(SContainer)`
   gap: 0;
-
-  background: radial-gradient(
-      48.95% 140.75% at 21.29% 131.36%,
-      rgba(255, 97, 144, 0.217) 0%,
-      rgba(73, 105, 132, 0.021) 100%,
-      rgba(132, 73, 91, 0.021) 100%
-    ),
-    rgba(66, 13, 38, 0.4);
 `
 
 export const SRewartCardHeader = styled.div`
