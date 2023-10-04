14d50e816ff05d7c278b25dc4014961f615b7525
Jakub Kolebaba <jakub@kolebaba.cz>
2023-10-04 10:38:19 +0200

    Fix stablepool types

--- src/api/stableswap.ts
+++ src/api/stableswap.ts
@@ -1,5 +1,3 @@
-// TODO: remove this before merging
-// @ts-nocheck
 import { useQuery } from "@tanstack/react-query"
 import { ApiPromise } from "@polkadot/api"
 import { QUERY_KEYS } from "utils/queryKeys"
@@ -22,6 +20,7 @@ export const useStableswapPool = (poolId: u32) => {
 export const getStableswapPools =
   (api: ApiPromise) => async (): Promise<{ id: u32; data: any }[]> => {
     const res = await api.query.stableswap.pools.entries()
+
     return res.map(([key, codec]) => {
       const [id] = key.args
       const data = codec.unwrap()
