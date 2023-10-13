46ef55df61815995c7bc1aa5f01adbd1c29ec562
Jakub Vonášek <jakub.vonasek@cleevio.com>
2023-10-13 15:55:47 +0200

    display all stableshare assets icons

--- src/components/MultipleIcons/MultipleIcons.styled.ts
+++ src/components/MultipleIcons/MultipleIcons.styled.ts
@@ -1,27 +1,58 @@
 import styled from "@emotion/styled"
 import { ResponsiveValue, getResponsiveStyles } from "utils/responsive"
 
-export const IconsWrapper = styled.div<{ size: ResponsiveValue<number> }>`
-  --additional-space: 1px;
+export const IconsWrapper = styled.div<{
+  size: ResponsiveValue<number>
+  maskConfig: Array<boolean>
+}>`
   ${({ size }) =>
-    getResponsiveStyles(size, (value) => ({ "--avatar-size": `${value}px` }))};
-  --mask-radius: calc(var(--avatar-size) / 2);
-  --circle-size: calc(var(--avatar-size) * 0.75);
+    getResponsiveStyles(size, (value) => ({
+      "--logo-size": `${value}px`,
+      "--logo-overlap": `${value * 0.3}px`,
+      "--chain-size": `${value / 2}px`,
+      "--chain-offset": `${value * 0.1}px`,
+    }))};
 
   position: relative;
 
   display: flex;
 
   & > span {
-    width: var(--avatar-size);
-    height: var(--avatar-size);
+    width: var(--logo-size);
+    height: var(--logo-size);
     > svg {
-      width: var(--avatar-size);
-      height: var(--avatar-size);
+      width: var(--logo-size);
+      height: var(--logo-size);
     }
   }
 
   > :not(:first-of-type) {
-    margin-left: -4px;
+    margin-left: calc(var(--logo-overlap) * -1);
   }
+
+  padding: var(--chain-offset) var(--chain-offset) 0 0;
+  margin-top: calc(var(--chain-offset) * -1);
+  margin-right: calc(var(--chain-offset) * -1);
+
+  --mask-space: 1px;
+  --mask-gradient: calc(var(--chain-size) / 2),
+    black calc(var(--chain-size) / 2 - 1px),
+    transparent calc(var(--chain-size) / 2 - 1px),
+    transparent calc(var(--chain-size) / 2 + var(--mask-space)),
+    black calc(var(--chain-size) / 2 + var(--mask-space) + 0.5px);
+
+  --mask-offset: calc(
+    var(--logo-size) - var(--chain-size) / 2 + var(--chain-offset)
+  );
+
+  mask: ${({ maskConfig }) => maskConfig.map(mapToMask).join(",")},
+    linear-gradient(black, black);
+
+  -webkit-mask-composite: destination-in;
+  mask-composite: exclude;
 `
+
+function mapToMask(hasMask: boolean, index: number) {
+  const shape = hasMask ? "circle" : "0 0"
+  return `radial-gradient(${shape} at calc(var(--logo-size) * ${index} - var(--logo-overlap) * ${index} + var(--mask-offset)) var(--mask-gradient))`
+}
--- src/sections/stats/components/PieChart/components/SliceLabel/SliceLabel.tsx
+++ src/sections/stats/components/PieChart/components/SliceLabel/SliceLabel.tsx
@@ -2,8 +2,10 @@ import BigNumber from "bignumber.js"
 import { AssetLogo } from "components/AssetIcon/AssetIcon"
 import { DisplayValue } from "components/DisplayValue/DisplayValue"
 import { Icon } from "components/Icon/Icon"
+import { MultipleIcons } from "components/MultipleIcons/MultipleIcons"
 import { Text } from "components/Typography/Text/Text"
 import { motion } from "framer-motion"
+import { useRpcProvider } from "providers/rpcProvider"
 import { useTranslation } from "react-i18next"
 
 type SliceLabelProps = {
@@ -21,6 +23,11 @@ export const SliceLabel = ({
 }: SliceLabelProps) => {
   const { t } = useTranslation()
 
+  const { assets } = useRpcProvider()
+
+  const meta = assets.getAsset(id)
+  const iconIds = assets.isStableSwap(meta) ? meta.assets : meta.id
+
   return (
     <motion.div
       sx={{ flex: "column", align: "center", gap: 6 }}
@@ -28,7 +35,16 @@ export const SliceLabel = ({
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.3 }}
     >
-      <Icon size={[20, 36]} icon={<AssetLogo id={id} />} />
+      {typeof iconIds === "string" ? (
+        <Icon size={[20, 36]} icon={<AssetLogo id={iconIds} />} />
+      ) : (
+        <MultipleIcons
+          size={[20, 36]}
+          icons={iconIds.map((id) => ({
+            icon: <AssetLogo id={id} />,
+          }))}
+        />
+      )}
       <Text color="basic100" fs={[20, 34]}>
         {t("value.percentage", { value: percentage })}
       </Text>
