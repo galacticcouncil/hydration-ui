bf9569d978fed1e53e918b0df0a7dd2f30e71dfe
vkulinich <viacheslav.kulinich@cleevio.com>
2023-11-29 20:29:49 +0100

    Enable buy and sell buttons

--- src/sections/wallet/assets/table/data/WalletAssetsTableData.utils.ts
+++ src/sections/wallet/assets/table/data/WalletAssetsTableData.utils.ts
@@ -135,8 +135,8 @@ export const useAssetsTableData = (isAllAssets: boolean) => {
       )
 
       const tradability = {
-        canBuy: !!tradabilityData?.canBuy,
-        canSell: !!tradabilityData?.canSell,
+        canBuy: tradabilityData?.canBuy ?? true,
+        canSell: tradabilityData?.canSell ?? true,
         canAddLiquidity: !!tradabilityData?.canAddLiquidity,
         canRemoveLiquidity: !!tradabilityData?.canRemoveLiquidity,
         inTradeRouter,
