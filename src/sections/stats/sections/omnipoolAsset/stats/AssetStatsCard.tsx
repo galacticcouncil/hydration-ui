0ce318f627b9d4aa3505ed81eb848433d6fe57e5
vkulinich <viacheslav.kulinich@cleevio.com>
2023-12-13 10:06:15 +0100

    Update apy label

--- src/sections/stats/sections/omnipoolAsset/stats/AssetStatsCard.tsx
+++ src/sections/stats/sections/omnipoolAsset/stats/AssetStatsCard.tsx
@@ -3,21 +3,33 @@ import { SCardContainer } from "./AssetStatsCard.styled"
 import Skeleton from "react-loading-skeleton"
 import { useMedia } from "react-use"
 import { theme } from "theme"
+import { InfoTooltip } from "components/InfoTooltip/InfoTooltip"
+import { SInfoIcon } from "sections/pools/pool/Pool.styled"
 
 export const AssetStatsCard = ({
   title,
   value,
   loading,
+  tooltip,
 }: {
   title: string
   value?: string
   loading?: boolean
+  tooltip?: string
 }) => {
   const isDesktop = useMedia(theme.viewport.gte.sm)
 
   return (
     <SCardContainer>
-      <Text color="brightBlue300">{title}</Text>
+      <div sx={{ flex: "row", gap: 4, align: "center" }}>
+        <Text color="brightBlue300">{title}</Text>{" "}
+        {tooltip && (
+          <InfoTooltip text={tooltip}>
+            <SInfoIcon />
+          </InfoTooltip>
+        )}
+      </div>
+
       {loading || !value ? (
         <Skeleton width={150} height={isDesktop ? 34 : 14} />
       ) : (
