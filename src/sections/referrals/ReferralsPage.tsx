1e64d8785dc5bc47e0df15d955720f81b79eab32
vkulinich <viacheslav.kulinich@cleevio.com>
2024-01-03 14:08:23 +0100

    Update font

--- src/sections/referrals/ReferralsPage.tsx
+++ src/sections/referrals/ReferralsPage.tsx
@@ -1,6 +1,5 @@
 import { Navigate } from "@tanstack/react-location"
 import { Page } from "components/Layout/Page/Page"
-import { Spacer } from "components/Spacer/Spacer"
 import { CodeForm } from "./components/CodeForm/CodeForm"
 import { ReferralsTableTableWrapper } from "./components/ReferralsTable/ReferralsTableWrapper"
 import { FaqAccordion } from "./components/FaqAccordion/FaqAccordion"
@@ -33,29 +32,27 @@ export const ReferralsPage = () => {
   const myReferralCode = userReferralCode.data?.[0]?.referralCode
 
   return (
-    <Page>
-      <HeroBanner>
-        <CodeForm />
-      </HeroBanner>
-      <Spacer size={30} />
-      {account && (
-        <>
-          <div sx={{ flex: ["column", "row"], gap: 20, flexWrap: "wrap" }}>
-            <RewardsCard />
-            <ReferrerCard />
-          </div>
-          {myReferralCode && (
-            <>
-              <Spacer size={30} />
-              <TierStats />
-              <Spacer size={30} />
-              <ReferralsTableTableWrapper />
-            </>
-          )}
-        </>
-      )}
-      <Spacer size={30} />
-      <FaqAccordion />
+    <Page css={{ display: "flex", flex: "column", gap: 30 }}>
+      <div sx={{ flex: "column", gap: 30 }}>
+        <HeroBanner>
+          <CodeForm />
+        </HeroBanner>
+        {account && (
+          <>
+            <div sx={{ flex: ["column", "row"], gap: 20, flexWrap: "wrap" }}>
+              <RewardsCard />
+              <ReferrerCard />
+            </div>
+            {myReferralCode && (
+              <>
+                <TierStats />
+                <ReferralsTableTableWrapper />
+              </>
+            )}
+          </>
+        )}
+        <FaqAccordion />
+      </div>
     </Page>
   )
 }
