2f31bd78d739429a1e4ee522dee22849fb9cf4fa
Pavol Noha <pavol.noha@gmail.com>
2024-01-15 22:08:16 +0100

    yield dca dapp

--- src/sections/trade/sections/yieldDca/YieldDcaPage.tsx
+++ src/sections/trade/sections/yieldDca/YieldDcaPage.tsx
@@ -1,3 +1,93 @@
-export const YieldDcaPage = () => {
-  return null
+import { SContainer } from "./YieldDcaPage.styled"
+
+import type { TxInfo } from "@galacticcouncil/apps"
+
+import * as React from "react"
+import * as Apps from "@galacticcouncil/apps"
+import { createComponent, EventName } from "@lit-labs/react"
+import { useStore } from "state/store"
+import { useProviderRpcUrlStore } from "api/provider"
+import { useRpcProvider } from "providers/rpcProvider"
+import { useAccount } from "sections/web3-connect/Web3Connect.utils"
+import { useDisplayAssetStore } from "utils/displayAsset"
+
+export const DcaYieldApp = createComponent({
+  tagName: "gc-dca-y-app",
+  elementClass: Apps.DcaApp,
+  react: React,
+  events: {
+    onDcaSchedule: "gc:tx:scheduleDca" as EventName<CustomEvent<TxInfo>>,
+    onDcaTerminate: "gc:tx:terminateDca" as EventName<CustomEvent<TxInfo>>,
+  },
+})
+
+const indexerUrl = import.meta.env.VITE_INDEXER_URL
+const grafanaUrl = import.meta.env.VITE_GRAFANA_URL
+const grafanaDsn = import.meta.env.VITE_GRAFANA_DSN
+const stableCoinAssetId = import.meta.env.VITE_STABLECOIN_ASSET_ID
+
+export function YieldDcaPage() {
+  const { api } = useRpcProvider()
+  const { account } = useAccount()
+  const { createTransaction } = useStore()
+  const { stableCoinId } = useDisplayAssetStore()
+
+  const preference = useProviderRpcUrlStore()
+  const rpcUrl = preference.rpcUrl ?? import.meta.env.VITE_PROVIDER_URL
+
+  const handleSubmit = async (e: CustomEvent<TxInfo>) => {
+    const { transaction, notification } = e.detail
+    await createTransaction(
+      {
+        tx: api.tx(transaction.hex),
+      },
+      {
+        onSuccess: () => {},
+        onSubmitted: () => {},
+        toast: {
+          onLoading: (
+            <span
+              dangerouslySetInnerHTML={{
+                __html: notification.processing.rawHtml,
+              }}
+            />
+          ),
+          onSuccess: (
+            <span
+              dangerouslySetInnerHTML={{
+                __html: notification.success.rawHtml,
+              }}
+            />
+          ),
+          onError: (
+            <span
+              dangerouslySetInnerHTML={{
+                __html: notification.failure.rawHtml,
+              }}
+            />
+          ),
+        },
+      },
+    )
+  }
+
+  return (
+    <SContainer>
+      <DcaYieldApp
+        ref={(r) => {
+          r && r.setAttribute("chart", "")
+        }}
+        apiAddress={rpcUrl}
+        stableCoinAssetId={stableCoinId ?? stableCoinAssetId}
+        accountName={account?.name}
+        accountProvider={account?.provider}
+        accountAddress={account?.address}
+        indexerUrl={indexerUrl}
+        grafanaUrl={grafanaUrl}
+        grafanaDsn={grafanaDsn}
+        onDcaSchedule={(e) => handleSubmit(e)}
+        onDcaTerminate={(e) => handleSubmit(e)}
+      />
+    </SContainer>
+  )
 }
