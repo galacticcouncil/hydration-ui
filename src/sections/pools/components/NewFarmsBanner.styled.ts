812bb42357b4a6917be77e52198e752b9e2af38e
vkulinich <viacheslav.kulinich@cleevio.com>
2024-01-17 12:10:06 +0100

    Add Farms banner

---
+++ src/sections/pools/components/NewFarmsBanner.styled.ts
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled"
+
+export const NewFarmsBannerContainer = styled.div`
+  background: linear-gradient(90deg, #18a8ff 0%, #057dd3 52.07%);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 14px;
+
+  padding: 6px 0;
+`
