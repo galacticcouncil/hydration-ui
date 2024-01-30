d3833ea068b24c81325dcd849646cde5f4e88406
vkulinich <viacheslav.kulinich@cleevio.com>
2024-01-30 13:19:41 +0100

    Fix mob layout

--- src/sections/pools/pool/Pool.styled.ts
+++ src/sections/pools/pool/Pool.styled.ts
@@ -4,7 +4,6 @@ import { theme } from "theme"
 export const SPoolContainer = styled.div`
   width: calc(100% + 24px);
 
-  border-radius: ${theme.borderRadius.medium}px;
   border: 1px solid rgba(152, 176, 214, 0.27);
   background-color: ${theme.colors.darkBlue700};
 
@@ -13,10 +12,14 @@ export const SPoolContainer = styled.div`
   margin: 0 -12px;
 
   position: relative;
+  height: 100%;
 
   @media ${theme.viewport.gte.sm} {
     margin: 0 auto;
 
     width: 730px;
+    height: auto;
+
+    border-radius: ${theme.borderRadius.medium}px;
   }
 `
