b8914490a8a223209ddcbe0ec41989d37326b618
vkulinich <viacheslav.kulinich@cleevio.com>
2024-05-31 13:45:10 +0200

    Rename HydraDX to Hydration

--- src/components/AssetIcon/AssetIcon.tsx
+++ src/components/AssetIcon/AssetIcon.tsx
@@ -53,30 +53,6 @@ export const UigcChainLogo = createComponent({
   react: React,
 })
 
-export function getAssetName(symbol: string | null | undefined) {
-  const _symbol = symbol?.toUpperCase()
-
-  if (_symbol === "AUSD") return "Acala Dollar"
-  if (_symbol === "BSX") return "Basilisk"
-  if (_symbol === "KAR") return "Karura"
-  if (_symbol === "KSM") return "Kusama"
-  if (_symbol === "PHA") return "Phala"
-  if (_symbol === "TNKR") return "Tinkernet"
-  if (_symbol === "HDX") return "HydraDX"
-  if (_symbol === "LRNA") return "Lerna"
-  if (_symbol === "DAI") return "Dai"
-  if (_symbol === "DOT") return "Polkadot"
-  if (_symbol === "BTC") return "Bitcoin"
-  if (_symbol === "ETH") return "Ethereum"
-  if (_symbol === "USDC") return "USD Coin"
-  if (_symbol === "USDT") return "Tether"
-  if (_symbol === "APE") return "ApeCoin"
-  if (_symbol === "ASTR") return "Astar"
-  if (_symbol === "IBTC") return "interBTC"
-
-  return "N/A"
-}
-
 export const AssetLogo = ({ id }: { id?: string }) => {
   const { t } = useTranslation()
   const { assets } = useRpcProvider()
