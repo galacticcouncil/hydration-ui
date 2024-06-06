8d97617b21cc89ff03a8e8b2b303eebf97daf1d1
Jakub Vonášek <jakub.vonasek@cleevio.com>
2024-06-06 11:19:31 +0200

    Updated meta images

--- src/seo.ts
+++ src/seo.ts
@@ -11,9 +11,9 @@ export const SEO_METADATA = {
     title: "Hydration - An Ocean of Liquidity",
     description:
       "Hydration is a next-gen DeFi protocol which is designed to bring an ocean of liquidity to Polkadot. Our tool for the job the Hydration - an innovative Automated Market Maker (AMM) which unlocks unparalleled efficiencies by combining all assets in a single trading pool.",
-    image: "https://app.hydration.net/images/meta-image.jpg",
+    image: "https://hydration.net/opengraph-image.png",
   },
   referrals: {
-    image: "https://hydration.net/assets/meta-referrals-image.jpg",
+    image: "https://hydration.net/opengraph-image-ref.png",
   },
 } satisfies MetadataMap
