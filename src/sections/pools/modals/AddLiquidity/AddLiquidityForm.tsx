0a6d07181da66cd212e4aaa1ede0321dec4739bf
vkulinich <viacheslav.kulinich@cleevio.com>
2024-08-01 16:59:56 +0200

    fix button disable

--- src/sections/pools/modals/AddLiquidity/AddLiquidityForm.tsx
+++ src/sections/pools/modals/AddLiquidity/AddLiquidityForm.tsx
@@ -293,7 +293,7 @@ export const AddLiquidityForm = ({
         type="submit"
         disabled={
           limits?.cap === false ||
-          !form.formState.errors ||
+          !!Object.keys(form.formState.errors).length ||
           !limits?.circuitBreaker.isWithinLimit
         }
       >
