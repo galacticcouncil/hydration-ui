e207732eca25c0678686188f950715bd3e8d96f8
Jakub Vonášek <vonasek.jakub@gmail.com>
2024-11-19 18:23:03 +0100

    aToken mainnet config

--- src/sections/lending/ui-config/aTokens.tsx
+++ src/sections/lending/ui-config/aTokens.tsx
@@ -8,13 +8,22 @@ export const A_TOKEN_UNDERLYING_ID_MAP_TESTNET: { [key: string]: string } = {
   "1000039": "10",
   // aUSDC
   "1000038": "21",
-  // aWETH
-  "1000041": "20",
   // aWBTC
   "1000040": "3",
+  // aWETH
+  "1000041": "20",
 }
 
-export const A_TOKEN_UNDERLYING_ID_MAP_MAINNET: { [key: string]: string } = {}
+export const A_TOKEN_UNDERLYING_ID_MAP_MAINNET: { [key: string]: string } = {
+  // aDOT
+  "1001": "5",
+  // aUSDT
+  "1002": "10",
+  // aUSDC
+  "1003": "21",
+  // aWBTC
+  "1004": "3",
+}
 
 export const A_TOKEN_UNDERLYING_ID_MAP =
   import.meta.env.VITE_ENV === "production"
