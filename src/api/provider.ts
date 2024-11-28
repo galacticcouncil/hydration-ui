2469e00d55a554b274cf99a213b8f1b83caeae2b
Pavol Noha <pavol.noha@gmail.com>
2024-11-28 10:42:25 +0100

    fix build

--- src/api/provider.ts
+++ src/api/provider.ts
@@ -5,12 +5,14 @@ import { persist } from "zustand/middleware"
 import { SubstrateApis } from "@galacticcouncil/xcm-core"
 import { useMemo } from "react"
 import { useShallow } from "hooks/useShallow"
-import { omit, pick } from "utils/rx"
+import { pick } from "utils/rx"
 import { ApiPromise, WsProvider } from "@polkadot/api"
 import { useRpcProvider } from "providers/rpcProvider"
 import {
+  Asset,
   AssetClient,
   BalanceClient,
+  findNestedKey,
   PoolService,
   PoolType,
   TradeRouter,
@@ -21,6 +23,7 @@ import { undefinedNoop } from "utils/helpers"
 import { ExternalAssetCursor } from "@galacticcouncil/apps"
 import { getPendulumAssetIdFromGeneralKey } from "utils/externalAssets"
 import { pendulum } from "./external/pendulum"
+import { assethub } from "./external/assethub"
 import { pingRpc } from "utils/rpc"
 
 export type TEnv = "testnet" | "mainnet"
@@ -147,6 +150,34 @@ export const useProviderAssets = () => {
   const { data: provider } = useProviderData()
   const rpcUrlList = useActiveRpcUrlList()
 
+  const getParachainId = (asset: Asset) => {
+    const entry = findNestedKey(asset.location, "parachain")
+    return entry && entry.parachain
+  }
+
+  const getGeneralIndex = (asset: Asset) => {
+    const entry = findNestedKey(asset.location, "generalIndex")
+    return entry && entry.generalIndex.toString()
+  }
+
+  const getGeneralKey = (asset: Asset) => {
+    const entry = findNestedKey(asset.location, "generalKey")
+    return entry && entry.generalKey
+  }
+
+  const getExternalId = (asset: Asset) => {
+    const parachainId = getParachainId(asset)
+    switch (parachainId) {
+      case pendulum.parachainId:
+        const generalKey = getGeneralKey(asset)
+        return getPendulumAssetIdFromGeneralKey(generalKey)
+      case assethub.parachainId:
+        return getGeneralIndex(asset)
+      default:
+        return undefined
+    }
+  }
+
   return useQuery(
     QUERY_KEYS.assets(rpcUrlList.join()),
     provider
@@ -180,15 +211,11 @@ export const useProviderAssets = () => {
                 (tradeAsset) => tradeAsset.id === asset.id,
               )
               return {
-                ...omit(["externalId"], asset),
+                ...asset,
                 symbol: asset.symbol ?? "",
                 decimals: asset.decimals ?? 0,
                 name: asset.name ?? "",
-                externalId:
-                  asset.origin === pendulum.parachainId &&
-                  typeof asset.externalId === "object"
-                    ? getPendulumAssetIdFromGeneralKey(asset.externalId)
-                    : asset.externalId?.toString(),
+                externalId: getExternalId(asset),
                 isTradable,
               }
             }),
