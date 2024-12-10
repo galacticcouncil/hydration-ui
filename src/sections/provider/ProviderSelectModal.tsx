c9ed8e5592bbc758838fb4a97199417f23ca7304
Jakub Vonášek <vonasek.jakub@gmail.com>
2024-12-10 10:50:10 +0100

    add active RPC url to rpc modal when in auto mode

--- src/sections/provider/ProviderSelectModal.tsx
+++ src/sections/provider/ProviderSelectModal.tsx
@@ -1,4 +1,4 @@
-import { useProviderRpcUrlStore } from "api/provider"
+import { useActiveProvider, useProviderRpcUrlStore } from "api/provider"
 import { Modal } from "components/Modal/Modal"
 import { useState } from "react"
 
@@ -7,18 +7,27 @@ import { Text } from "components/Typography/Text/Text"
 import { useTranslation } from "react-i18next"
 import { ProviderSelectForm } from "sections/provider/ProviderSelectForm"
 import { useRpcStore } from "state/store"
-import { SSwitchContainer } from "./ProviderSelectModal.styled"
+import {
+  SAutoModeActiveContainer,
+  SSwitchContainer,
+  SSWitchContent,
+} from "./ProviderSelectModal.styled"
 import { DeleteModal } from "./components/DeleteModal/DeleteModal"
 import { Button } from "components/Button/Button"
+import { ProviderItem } from "sections/provider/components/ProviderItem/ProviderItem"
+import { useRpcProvider } from "providers/rpcProvider"
 
 export function ProviderSelectModal(props: {
   open: boolean
   onClose: () => void
 }) {
+  const { isLoaded } = useRpcProvider()
   const { setRpcUrl, autoMode, setAutoMode } = useProviderRpcUrlStore()
   const [removeRpcUrl, setRemoveRpcUrl] = useState<string | undefined>()
   const { t } = useTranslation()
   const { removeRpc } = useRpcStore()
+
+  const activeProvider = useActiveProvider()
 
   return (
     <>
@@ -28,34 +37,44 @@ export function ProviderSelectModal(props: {
         title={t("rpc.change.modal.title")}
         headerVariant="GeistMono"
       >
-        <SSwitchContainer
-          sx={{
-            flex: "row",
-            align: "center",
-            justify: "space-between",
-            gap: 10,
-          }}
-        >
-          <div sx={{ maxWidth: 280 }}>
-            <Text fs={13} sx={{ mb: 4 }}>
-              {t("rpc.change.modal.autoMode.title")}
-            </Text>
-            <Text color="basic400" fs={12}>
-              {t("rpc.change.modal.autoMode.desc")}
-            </Text>
-          </div>
-          <div sx={{ flex: "row", align: "center", gap: 8 }}>
-            <Text fs={11} color="basic400">
-              {autoMode
-                ? t("rpc.change.modal.autoMode.enabled")
-                : t("rpc.change.modal.autoMode.disabled")}
-            </Text>
-            <Switch
-              name="rpc-auto-mode"
-              value={autoMode}
-              onCheckedChange={setAutoMode}
-            />
-          </div>
+        <SSwitchContainer>
+          <SSWitchContent>
+            <div sx={{ maxWidth: 280 }}>
+              <Text fs={13} sx={{ mb: 4 }}>
+                {t("rpc.change.modal.autoMode.title")}
+              </Text>
+              <Text color="basic400" fs={12}>
+                {t("rpc.change.modal.autoMode.desc")}
+              </Text>
+            </div>
+            <div sx={{ flex: "row", align: "center", gap: 8 }}>
+              <Text fs={11} color="basic400">
+                {autoMode
+                  ? t("rpc.change.modal.autoMode.enabled")
+                  : t("rpc.change.modal.autoMode.disabled")}
+              </Text>
+              <Switch
+                name="rpc-auto-mode"
+                value={autoMode}
+                onCheckedChange={setAutoMode}
+              />
+            </div>
+          </SSWitchContent>
+          {autoMode && activeProvider.url && (
+            <SAutoModeActiveContainer sx={{ mt: 14 }}>
+              <ProviderItem
+                sx={{ opacity: isLoaded ? 1 : 0 }}
+                name={activeProvider.name}
+                url={activeProvider.url}
+                isActive
+              />
+              {!isLoaded && (
+                <Button variant="outline" isLoading>
+                  {t("connecting")}
+                </Button>
+              )}
+            </SAutoModeActiveContainer>
+          )}
         </SSwitchContainer>
 
         <div sx={{ pt: 12 }}>
