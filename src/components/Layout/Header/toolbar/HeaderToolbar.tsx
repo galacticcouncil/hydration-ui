8aa2d4d8cee495c07b8455e27fa92e4bb4a4e2e6
Jakub Vonášek <vonasek.jakub@gmail.com>
2024-12-17 14:02:07 +0100

    deposit button

--- src/components/Layout/Header/toolbar/HeaderToolbar.tsx
+++ src/components/Layout/Header/toolbar/HeaderToolbar.tsx
@@ -7,6 +7,8 @@ import { Web3ConnectModalButton } from "sections/web3-connect/modal/Web3ConnectM
 import { useMatchRoute, useNavigate } from "@tanstack/react-location"
 import { LINKS } from "utils/navigation"
 import { Button } from "components/Button/Button"
+import DownloadIcon from "assets/icons/DownloadIcon.svg?react"
+import { Icon } from "components/Icon/Icon"
 
 export const HeaderToolbar = () => {
   const isSmallMedia = useMedia(theme.viewport.lt.sm)
@@ -17,14 +19,19 @@ export const HeaderToolbar = () => {
 
   return (
     <div sx={{ flex: "row", align: "center", gap: 14 }}>
-      <div sx={{ flex: "row", gap: 10 }}>
+      <div sx={{ flex: "row", gap: 10, align: "center" }}>
         {!isSmallMedia && <Documentation />}
         <Bell />
         {!isSmallMedia && !isSubmitTransactionPath && <Settings />}
+        <Button
+          size="compact"
+          variant="mutedSecondary"
+          onClick={() => navigate({ to: LINKS.deposit })}
+        >
+          <Icon size={18} sx={{ ml: -4 }} icon={<DownloadIcon />} />
+          Deposit
+        </Button>
       </div>
-      <Button size="small" onClick={() => navigate({ to: LINKS.deposit })}>
-        Deposit
-      </Button>
       <Web3ConnectModalButton size="small" css={{ maxHeight: 40 }} />
     </div>
   )
