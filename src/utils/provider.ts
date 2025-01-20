be210ecc7bfba3067f7db0037d6bbc2cc9c62627
Jakub Vonášek <vonasek.jakub@gmail.com>
2025-01-20 13:14:53 +0100

    refactor pjs evm provider

--- src/utils/provider.ts
+++ src/utils/provider.ts
@@ -1,11 +1,12 @@
 import { getNetwork, JsonRpcProvider, Network } from "@ethersproject/providers"
 import { ApiPromise, WsProvider } from "@polkadot/api"
+import { getProviderInstance } from "api/provider"
 
 export class PolkadotEvmRpcProvider extends JsonRpcProvider {
   provider: WsProvider
 
   constructor(api: ApiPromise) {
-    const provider = PolkadotEvmRpcProvider.getProviderInstance(api)
+    const provider = getProviderInstance(api)
     const path = provider.endpoint
     super(path)
     this.provider = provider
@@ -14,12 +15,6 @@ export class PolkadotEvmRpcProvider extends JsonRpcProvider {
   async _uncachedDetectNetwork(): Promise<Network> {
     const chainId = await this.send("eth_chainId", [])
     return getNetwork(parseInt(chainId, 16))
-  }
-
-  static getProviderInstance(api: ApiPromise) {
-    // @ts-expect-error Property '_options' is protected
-    const options = api?._options
-    return options?.provider as WsProvider
   }
 
   send(method: string, params: Array<any> = []): Promise<any> {
