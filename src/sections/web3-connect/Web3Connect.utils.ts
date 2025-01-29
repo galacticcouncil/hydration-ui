16a2b5d89813b0659bb4e7ca51e966b86bbb074f
Jakub Vonášek <vonasek.jakub@gmail.com>
2025-01-29 11:35:53 +0100

    Disconnect incompatible wallets on reload

--- src/sections/web3-connect/Web3Connect.utils.ts
+++ src/sections/web3-connect/Web3Connect.utils.ts
@@ -253,9 +253,15 @@ export const useWeb3ConnectEagerEnable = () => {
 
     if (providers.length > 0) {
       providers.forEach((p) => {
+        const isCompatible =
+          currentAccount?.provider === p.type ||
+          COMPATIBLE_WALLET_PROVIDERS.includes(p.type)
+
         const { wallet } = getWalletProviderByType(p.type)
-        if (wallet) {
+        if (wallet && isCompatible) {
           eagerEnable(wallet)
+        } else {
+          state.disconnect(p.type)
         }
       })
     } else {
