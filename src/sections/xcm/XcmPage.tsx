78a5142efc97730c6f99e822631d69ac7f468f8a
Pavol Noha <pavol.noha@gmail.com>
2025-02-03 13:26:29 +0200

    disable cex listings in ui

--- src/sections/xcm/XcmPage.tsx
+++ src/sections/xcm/XcmPage.tsx
@@ -141,7 +141,7 @@ export function XcmPage() {
         : undefined
   const ss58Prefix = genesisHashToChain(account?.genesisHash).prefix
 
-  const blacklist = import.meta.env.VITE_ENV === "production" ? "acala-evm" : ""
+  const blacklist = import.meta.env.VITE_ENV === "production" ? "assethub_cex,polkadot_cex" : ""
 
   useMount(() => {
     const srcChain = search?.data?.srcChain
