ca67d627316509af2d022c34dde962c662f5fad2
vkulinich <viacheslav.kulinich@cleevio.com>
2025-02-06 11:27:00 +0100

    Improve staking graph

--- src/sections/staking/StakingPage.utils.ts
+++ src/sections/staking/StakingPage.utils.ts
@@ -62,14 +62,25 @@ const getCurrentActionPoints = (
   votes: TStakingPosition["votes"],
   initialActionPoints: number,
   stakePosition: BN,
+  activeReferendaIds: string[],
 ) => {
   let currentActionPoints = 0
+  let maxActionPoints = 0
 
   const maxVotingPower = stakePosition.multipliedBy(CONVICTIONS["locked6x"])
   const maxActionPointsPerRef = 100
+  const maxConviction = CONVICTIONS["locked6x"]
+
+  const votedActiveReferendaIds: string[] = []
 
   votes?.forEach((vote) => {
     const convictionIndex = CONVICTIONS[vote.conviction.toLowerCase()]
+
+    const isActiveReferenda = activeReferendaIds.includes(vote.id.toString())
+
+    if (isActiveReferenda) {
+      votedActiveReferendaIds.push(vote.id.toString())
+    }
 
     currentActionPoints += Math.floor(
       vote.amount
@@ -78,6 +89,26 @@ const getCurrentActionPoints = (
         .div(maxVotingPower)
         .toNumber(),
     )
+
+    maxActionPoints += Math.floor(
+      vote.amount
+        .multipliedBy(isActiveReferenda ? maxConviction : convictionIndex)
+        .multipliedBy(maxActionPointsPerRef)
+        .div(maxVotingPower)
+        .toNumber(),
+    )
+  })
+
+  activeReferendaIds.forEach((activeReferendaId) => {
+    if (!votedActiveReferendaIds.includes(activeReferendaId)) {
+      maxActionPoints += Math.floor(
+        stakePosition
+          .multipliedBy(maxConviction)
+          .multipliedBy(maxActionPointsPerRef)
+          .div(maxVotingPower)
+          .toNumber(),
+      )
+    }
   })
 
   const actionMultipliers = {
@@ -87,7 +118,10 @@ const getCurrentActionPoints = (
   currentActionPoints *= actionMultipliers.democracyVote
   currentActionPoints += initialActionPoints ?? 0
 
-  return BN(currentActionPoints)
+  maxActionPoints *= actionMultipliers.democracyVote
+  maxActionPoints += initialActionPoints ?? 0
+
+  return { currentActionPoints, maxActionPoints }
 }
 
 export const useStakeData = () => {
@@ -376,7 +410,7 @@ export const useClaimReward = () => {
   const bestNumber = useBestNumber()
   const stake = useStake(account?.address)
   const stakingConsts = useStakingConsts()
-  const { data: openGovReferendas } = useOpenGovReferendas()
+  const { data: openGovReferendas = [] } = useOpenGovReferendas()
 
   const potAddress = getHydraAccountAddress(stakingConsts.data?.palletId)
   const potBalance = useTokenBalance(native.id, potAddress)
@@ -443,31 +477,37 @@ export const useClaimReward = () => {
       stakePosition.votes,
       stakePosition.actionPoints.toNumber(),
       stakePosition.stake,
+      openGovReferendas.map((referenda) => referenda.id),
     )
+
+    console.log(actionPoints)
 
     const points = wasm.calculate_points(
       enteredAt,
       currentPeriod,
       timePointsPerPeriod.toString(),
       timePointsWeight.toString(),
-      actionPoints.toString(),
+      actionPoints.currentActionPoints.toString(),
       actionPointsWeight.toString(),
       stakePosition.accumulatedSlashPoints.toString(),
     )
 
     let extraPayablePercentageHuman: string | undefined
-    if (openGovReferendas?.length) {
+    if (openGovReferendas.length) {
       const voteActionPoints = getVoteActionPoints(
         stakePosition.stake,
         openGovReferendas.length,
       )
-
+      console.log(
+        BN(actionPoints.currentActionPoints).plus(voteActionPoints).toString(),
+        actionPoints.maxActionPoints,
+      )
       const extraPoints = wasm.calculate_points(
         enteredAt,
         currentPeriod,
         timePointsPerPeriod.toString(),
         timePointsWeight.toString(),
-        actionPoints.plus(voteActionPoints).toString(),
+        actionPoints.maxActionPoints.toString(),
         actionPointsWeight.toString(),
         stakePosition.accumulatedSlashPoints.toString(),
       )
