0207b210af86e6ed0a8ca53417695222d35077be
vkulinich <viacheslav.kulinich@cleevio.com>
2025-02-10 11:16:12 +0100

    remove redundant code

--- apps/main/src/modules/trade/SwapPage.tsx
+++ apps/main/src/modules/trade/SwapPage.tsx
@@ -1,37 +1,14 @@
-import {
-  Flex,
-  Paper,
-  Separator,
-  Skeleton,
-  Text,
-} from "@galacticcouncil/ui/components"
+import { Flex, Paper, Separator } from "@galacticcouncil/ui/components"
 import { Outlet } from "@tanstack/react-router"
-
-import { useAssetsPrice } from "@/states/displayAsset"
 
 import { FormHeader } from "./components"
 import { SContainer } from "./SwapPage.styled"
-const keys = ["0", "5", "9", "22", "7", "8", "9"] //["5"] //
 
 export const SwapPage = () => {
-  const assets = useAssetsPrice(keys)
-
-  console.log(assets, "updated assets")
   return (
     <Flex gap={20} sx={{ flexWrap: "wrap" }} justify="center">
       <Flex direction="column" gap={20} sx={{ width: ["100%", 440] }}>
-        <Paper sx={{ height: 700, minWidth: ["auto", 640] }}>
-          {Object.entries(assets).map(([id, data]) => (
-            <Flex gap={24} key={id}>
-              <Text>{id}</Text>
-              {data.isLoading ? (
-                <Skeleton width={30} height={15} />
-              ) : (
-                <Text>{data?.price}</Text>
-              )}
-            </Flex>
-          ))}
-        </Paper>
+        <Paper sx={{ height: 700, minWidth: ["auto", 640] }}></Paper>
         <SContainer>
           <FormHeader />
           <Separator mx={-20} />
