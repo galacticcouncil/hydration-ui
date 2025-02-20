bc8cfaf02dc199668ac06ed4eb4afc9275d426ef
Jakub Vonášek <vonasek.jakub@gmail.com>
2025-02-20 18:42:36 +0100

    EVM wallets RPC url update

--- src/utils/metamask.ts
+++ src/utils/metamask.ts
@@ -5,6 +5,8 @@ import type EventEmitter from "events"
 import UniversalProvider from "@walletconnect/universal-provider/dist/types/UniversalProvider"
 import { chainsMap } from "@galacticcouncil/xcm-cfg"
 import { EvmParachain } from "@galacticcouncil/xcm-core"
+import { PROVIDER_URLS, useProviderRpcUrlStore } from "api/provider"
+import { wsToHttp } from "utils/formatting"
 
 const METAMASK_LIKE_CHECKS = [
   "isTalisman",
@@ -48,10 +50,19 @@ const chainIconMap: { [key: string]: string[] } = {
 const getAddEvmChainParams = (chain: string): AddEvmChainParams => {
   const chainProps = (chainsMap.get(chain) as EvmParachain).client.chain
 
+  let rpcUrls = [...chainProps.rpcUrls.default.http]
+
+  if (chain === "hydration") {
+    const primaryRpcUrl = useProviderRpcUrlStore.getState().rpcUrl
+    rpcUrls = [...PROVIDER_URLS]
+      .sort((url) => (url === primaryRpcUrl ? -1 : 1))
+      .map(wsToHttp)
+  }
+
   return {
     chainId: "0x" + Number(chainProps.id).toString(16),
     chainName: chainProps.name,
-    rpcUrls: chainProps.rpcUrls.default.http as string[],
+    rpcUrls,
     iconUrls: chainIconMap[chain] || [],
     nativeCurrency: chainProps.nativeCurrency,
     blockExplorerUrls: chainProps.blockExplorers?.default
@@ -125,6 +136,7 @@ type RequestNetworkSwitchOptions = {
   onSwitch?: () => void
   chain?: string
 }
+
 export async function requestNetworkSwitch(
   provider: Maybe<MetaMaskLikeProvider>,
   options: RequestNetworkSwitchOptions = {},
@@ -134,6 +146,11 @@ export async function requestNetworkSwitch(
   const params = getAddEvmChainParams(options.chain ?? "hydration")
 
   try {
+    // request to add chain first, wallet will skip this if the chain and rpc combination already exists
+    await provider.request({
+      method: "wallet_addEthereumChain",
+      params: [params],
+    })
     await provider
       .request({
         method: "wallet_switchEthereumChain",
