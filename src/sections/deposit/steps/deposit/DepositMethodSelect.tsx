1adc1a03d02ccc33d83a5bb529188b9f6de5224c
Jakub Vonášek <vonasek.jakub@gmail.com>
2025-02-24 16:45:56 +0100

    Added withdraw/deposit icons

--- src/sections/deposit/steps/deposit/DepositMethodSelect.tsx
+++ src/sections/deposit/steps/deposit/DepositMethodSelect.tsx
@@ -4,6 +4,9 @@ import { useTranslation } from "react-i18next"
 import { StepButton } from "sections/deposit/components/StepButton"
 import { DepositMethod } from "sections/deposit/types"
 import { LINKS } from "utils/navigation"
+import ChainlinkIcon from "assets/icons/ChainlinkIcon.svg?react"
+import SuitcaseIcon from "assets/icons/SuitcaseIcon.svg?react"
+import CreditCardIcon from "assets/icons/CreditCardIcon.svg?react"
 
 type methodSelectProps = {
   onSelect: (method: DepositMethod) => void
@@ -24,16 +27,20 @@ export const DepositMethodSelect: React.FC<methodSelectProps> = ({
       </div>
       <div sx={{ flex: "column", gap: 20 }}>
         <StepButton
+          icon={SuitcaseIcon}
           onClick={() => onSelect(DepositMethod.DepositCex)}
           title={t("deposit.method.cex.title")}
           description={t("deposit.method.cex.description")}
         />
         <StepButton
+          icon={ChainlinkIcon}
           onClick={() => navigate({ to: LINKS.cross_chain })}
           title={t("deposit.method.onchain.title")}
           description={t("deposit.method.onchain.description")}
         />
+
         <StepButton
+          icon={CreditCardIcon}
           onClick={() => onSelect(DepositMethod.DepositCrypto)}
           title={t("deposit.method.crypto.title")}
           description={t("deposit.method.crypto.description")}
