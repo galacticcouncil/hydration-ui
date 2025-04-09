b5855c8d892199f93f8c62f4f25ce007db4207a8
Jakub Vonášek <vonasek.jakub@gmail.com>
2025-04-09 14:34:16 +0200

    Updated GDOT wallet banner

---
+++ src/sections/wallet/strategy/WalletStrategyBanner/WalletStrategyBanner.tsx
@@ -0,0 +1,39 @@
+import { Text } from "components/Typography/Text/Text"
+import { useTranslation } from "react-i18next"
+import { FC } from "react"
+
+import { Button } from "components/Button/Button"
+import { Link } from "@tanstack/react-location"
+import { LINKS } from "utils/navigation"
+import {
+  SCansImage,
+  SWalletStrategyBanner,
+} from "./WalletStrategyBanner.styled"
+import GigadotTitle from "./assets/gigadot-title.svg?react"
+import cansImageSrc from "./assets/cans.webp"
+
+export const WalletStrategyBanner: FC = () => {
+  const { t } = useTranslation()
+
+  return (
+    <SWalletStrategyBanner>
+      <SCansImage src={cansImageSrc} alt="Hydration" />
+      <div sx={{ flex: "column", gap: 6 }}>
+        <GigadotTitle
+          sx={{ color: "white" }}
+          aria-label={t("wallet.strategy.banner.title")}
+        />
+        <Text fw={400} fs={13} lh="1.3" color="white">
+          {t("wallet.strategy.banner.description")}
+        </Text>
+      </div>
+      <Button
+        size="small"
+        sx={{ width: ["100%", 120], ml: "auto" }}
+        as={(props) => <Link to={LINKS.walletStrategy} {...props} />}
+      >
+        {t("wallet.strategy.banner.cta")}
+      </Button>
+    </SWalletStrategyBanner>
+  )
+}
