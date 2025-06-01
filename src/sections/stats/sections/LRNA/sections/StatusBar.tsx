58067a6b115760b1d22e8c4ded1e4aa60eef6c1b
Pavol Noha <pavol.noha@gmail.com>
2025-06-01 13:28:13 +0200

    fix lint

--- src/sections/stats/sections/LRNA/sections/StatusBar.tsx
+++ src/sections/stats/sections/LRNA/sections/StatusBar.tsx
@@ -1,9 +1,5 @@
-import { useHubAssetImbalance } from "api/omnipool"
-import { Burning } from "sections/stats/sections/LRNA/components/Burning/Burning"
 import { Bidding } from "sections/stats/sections/LRNA/components/Bidding/Bidding"
 
 export const StatusBar = () => {
-  const imbalance = useHubAssetImbalance()
-
-  return imbalance.data?.negative?.toPrimitive() ? <Burning /> : <Bidding />
+  return <Bidding />
 }
