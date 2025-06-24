721f99d8f1564a0b4758f8b8fd502aecaf8fa870
Jakub Vonášek <vonasek.jakub@gmail.com>
2025-06-24 13:24:33 +0200

    wip

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -59,41 +59,39 @@ export const Slider: FC<SliderProps> = ({
   const [rootWidth, setRootWidth] = useState(0)
   const rootRef = useRef<HTMLSpanElement>(null)
 
-  const dashCount = dashes === "auto" ? Math.floor((max - min) / step) : dashes
+  const thumbSizePx = thumbSizeMap[thumbSize]
 
   const dashFormatter = useRef(formatDashValue)
   useEffect(() => {
     dashFormatter.current = formatDashValue
   }, [formatDashValue])
 
-  const thumbSizePx = thumbSizeMap[thumbSize]
+  const dashContent = useMemo(() => {
+    const count = dashes === "auto" ? Math.floor((max - min) / step) : dashes
 
-  const dashContent = useMemo(
-    () =>
-      Array.from({ length: dashCount + 1 }).map((_, i) => {
-        const dashValue = min + i * step
+    return Array.from({ length: count + 1 }).map((_, i) => {
+      const value = min + i * step
 
-        const percent = (100 * (dashValue - min)) / (max - min)
-        const thumbOffset = calculateThumbOffset(percent, thumbSizePx)
-        const dashOffset = thumbOffset * -1
-        const offset = i * (rootWidth / dashCount) - dashOffset
+      // if dash values are shown, align them at the center of the thumb
+      const thumbOffset = withDashValues
+        ? calculateThumbOffset(value, min, max, thumbSizePx)
+        : 0
 
-        return (
-          <Fragment key={i}>
-            <SDash key={`top-${i}`} offset={offset} row="top" />
-            <SDash key={`bottom-${i}`} offset={offset} row="bottom"></SDash>
-            {withDashValues && (
-              <SDashLabel offset={offset}>
-                {dashFormatter.current
-                  ? dashFormatter.current(dashValue)
-                  : dashValue}
-              </SDashLabel>
-            )}
-          </Fragment>
-        )
-      }),
-    [dashCount, max, min, rootWidth, step, thumbSizePx, withDashValues],
-  )
+      const offset = i * (rootWidth / count) - thumbOffset * -1
+
+      return (
+        <Fragment key={i}>
+          <SDash key={`top-${i}`} offset={offset} row="top" />
+          <SDash key={`bottom-${i}`} offset={offset} row="bottom"></SDash>
+          {withDashValues && (
+            <SDashLabel offset={offset}>
+              {dashFormatter.current ? dashFormatter.current(value) : value}
+            </SDashLabel>
+          )}
+        </Fragment>
+      )
+    })
+  }, [dashes, max, min, rootWidth, step, thumbSizePx, withDashValues])
 
   useLayoutEffect(() => {
     if (rootRef.current) setRootWidth(rootRef.current.offsetWidth)
@@ -128,7 +126,13 @@ function linearScale(input: [number, number], output: [number, number]) {
   }
 }
 
-function calculateThumbOffset(percent: number, thumbSize: number) {
+function calculateThumbOffset(
+  value: number,
+  min: number,
+  max: number,
+  thumbSize: number,
+) {
+  const percent = (100 * (value - min)) / (max - min)
   const halfWidth = thumbSize / 2
   const offset = linearScale([0, 50], [0, halfWidth])(percent)
   return halfWidth - offset
