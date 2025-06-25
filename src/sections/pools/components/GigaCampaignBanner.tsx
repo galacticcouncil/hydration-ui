e9e5a703dd7c4809de84af5a7e7afa25bbaca0de
vkulinich <viacheslav.kulinich@cleevio.com>
2025-06-25 14:41:40 +0200

    adjust textation

--- src/sections/pools/components/GigaCampaignBanner.tsx
+++ src/sections/pools/components/GigaCampaignBanner.tsx
@@ -2,15 +2,15 @@ import { Text } from "components/Typography/Text/Text"
 import { GigaBannerContainer } from "./GigaCampaignBanner.styled"
 import { useTranslation } from "react-i18next"
 import { Icon } from "components/Icon/Icon"
-import Title from "assets/icons/gigaCampaignTitle.svg?react"
+import Title from "assets/icons/GigaCampaignTitle.svg?react"
 
 export const GigaCampaignBanner = () => {
   const { t } = useTranslation()
 
   return (
     <GigaBannerContainer sx={{ mt: 12 }}>
-      <div sx={{ flex: "column", gap: 6 }}>
-        <Icon icon={<Title />} />
+      <div sx={{ flex: "column" }}>
+        <Icon icon={<Title />} sx={{ ml: -2 }} />
         <Text fs={13} color="black" css={{ textShadow: "0px 0px 5.9px #FFF" }}>
           {t("giga.campaign.description")}
         </Text>
