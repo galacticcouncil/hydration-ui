8e61d3ea0d370b11d0625a43d67a395b38508a45
vkulinich <viacheslav.kulinich@cleevio.com>
2025-06-25 14:53:21 +0200

    rename svg

--- src/sections/pools/components/GigaCampaignBanner.tsx
+++ src/sections/pools/components/GigaCampaignBanner.tsx
@@ -2,7 +2,7 @@ import { Text } from "components/Typography/Text/Text"
 import { GigaBannerContainer } from "./GigaCampaignBanner.styled"
 import { useTranslation } from "react-i18next"
 import { Icon } from "components/Icon/Icon"
-import Title from "assets/icons/GigaCampaignTitle.svg?react"
+import Title from "assets/icons/gigaCampaignTitle.svg?react"
 
 export const GigaCampaignBanner = () => {
   const { t } = useTranslation()
