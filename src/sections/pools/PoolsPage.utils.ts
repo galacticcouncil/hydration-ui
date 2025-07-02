56b821fb715639f5ebfbfc76d7e5b1558a6c1990
vkulinich <viacheslav.kulinich@cleevio.com>
2025-07-02 13:39:54 +0200

    debug1

--- src/sections/pools/PoolsPage.utils.ts
+++ src/sections/pools/PoolsPage.utils.ts
@@ -215,21 +215,21 @@ export const usePools = () => {
   const omnipoolAssets = useOmnipoolDataObserver()
   //const { data: accountAssets } = useAccountAssets()
 
-  // const { data: stablepools, isLoading: isLoadingStablepools } =
-  //   useStablepools()
+  const { data: stablepools, isLoading: isLoadingStablepools } =
+    useStablepools()
 
-  // const assetsId = useMemo(
-  //   () => omnipoolAssets.data?.map((a) => a.id) ?? [],
-  //   [omnipoolAssets.data],
-  // )
+  const assetsId = useMemo(
+    () => omnipoolAssets.data?.map((a) => a.id) ?? [],
+    [omnipoolAssets.data],
+  )
 
   // const { data: allFarms, isLoading: isAllFarmsLoading } =
   //   useOmnipoolFarms(assetsId)
 
-  // const { isLoading, getAssetPrice } = useAssetsPrice(assetsId)
+  const { isLoading, getAssetPrice } = useAssetsPrice(assetsId)
 
-  // const isInitialLoading =
-  //   omnipoolAssets.isLoading || isLoading || isLoadingStablepools
+  const isInitialLoading =
+    omnipoolAssets.isLoading || isLoading || isLoadingStablepools
 
   // const { data: volumes, isLoading: isVolumeLoading } =
   //   useOmnipoolVolumes(isInitialLoading)
