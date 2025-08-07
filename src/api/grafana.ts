05caf3cbef921df69bb2fbf3c9e5cd3e58fd9e2a
Radoslav Karlík <radoslav.karlik@cleevio.com>
2025-08-07 08:51:35 +0200

    f/HYDRA-1164 - Money market reserve charts

---
+++ src/api/grafana.ts
@@ -0,0 +1,34 @@
+const grafanaUrl = import.meta.env.VITE_GRAFANA_URL
+const grafanaDsn = Number(import.meta.env.VITE_GRAFANA_DSN)
+
+export const grafanaQuery = async (
+  sql: string,
+  refId: "buckets" | "price",
+  signal?: AbortSignal,
+) =>
+  fetch(grafanaUrl, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({
+      queries: [
+        {
+          refId,
+          rawSql: sql,
+          format: "table",
+          datasourceId: grafanaDsn,
+        },
+      ],
+    }),
+    signal,
+  })
+    .then(async (response) => {
+      if (!response.ok) {
+        throw new Error(response.status + "\n" + (await response.text()))
+      }
+
+      return response.json()
+    })
+    .then((data) => data.results[refId].frames[0].data.values)
