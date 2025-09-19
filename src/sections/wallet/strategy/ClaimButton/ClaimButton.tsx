ae5cfe343b469da4df920b8e1f4a56b50dbdc5b9
Jakub Vonášek <vonasek.jakub@gmail.com>
2025-09-19 14:04:33 +0200

    Claim USD value fix

--- src/sections/wallet/strategy/ClaimButton/ClaimButton.tsx
+++ src/sections/wallet/strategy/ClaimButton/ClaimButton.tsx
@@ -1,3 +1,4 @@
+import BN from "bignumber.js"
 import { Button } from "components/Button/Button"
 import { DataValue } from "components/DataValue"
 import { Text } from "components/Typography/Text/Text"
@@ -5,13 +6,23 @@ import { useTranslation } from "react-i18next"
 import { useMedia } from "react-use"
 import { useClaimGdotReward } from "sections/wallet/strategy/ClaimButton/ClaimButton.utils"
 import { theme } from "theme"
+import { BN_0 } from "utils/constants"
 
 export const ClaimButton = () => {
   const isMobile = useMedia(theme.viewport.lt.sm)
   const { t } = useTranslation()
-  const { action, reward } = useClaimGdotReward()
 
-  if (!reward || reward.balanceUsd === "0") return null
+  const { action, incentive } = useClaimGdotReward()
+
+  const rewardBalance = incentive
+    ? BN(incentive.claimableRewards).shiftedBy(-incentive.rewardTokenDecimals)
+    : BN_0
+
+  const rewardBalanceUsd = incentive
+    ? rewardBalance.times(incentive?.rewardPriceFeed)
+    : BN_0
+
+  if (!rewardBalanceUsd.gt(0)) return null
 
   if (isMobile) {
     return (
@@ -30,7 +41,7 @@ export const ClaimButton = () => {
         <div sx={{ flex: "row", align: "center", gap: 10 }}>
           <Text fs={16}>
             {t("value.usd", {
-              amount: reward.balanceUsd,
+              amount: rewardBalanceUsd,
             })}
           </Text>
           <Button variant="primary" size="micro" onClick={() => action()}>
@@ -49,7 +60,7 @@ export const ClaimButton = () => {
       <div sx={{ flex: "row", align: "center", justify: "flex-end", gap: 10 }}>
         <Text fs={16}>
           {t("value.usd", {
-            amount: reward.balanceUsd,
+            amount: rewardBalanceUsd,
           })}
         </Text>
         <Button variant="primary" size="micro" onClick={() => action()}>
--- src/sections/wallet/strategy/ClaimButton/ClaimButton.utils.ts
+++ src/sections/wallet/strategy/ClaimButton/ClaimButton.utils.ts
@@ -1,11 +1,16 @@
 import { ProtocolAction } from "@aave/contract-helpers"
 import { useTransactionHandler } from "sections/lending/helpers/useTransactionHandler"
+import { useAppDataContext } from "sections/lending/hooks/app-data-provider/useAppDataProvider"
 import { useRootStore } from "sections/lending/store/root"
 import { useUserRewards } from "sections/wallet/strategy/StrategyTile/StrategyTile.data"
 import { GDOT_STABLESWAP_ASSET_ID } from "utils/constants"
 
 export const useClaimGdotReward = () => {
+  const { user, loading } = useAppDataContext()
   const reward = useUserRewards([GDOT_STABLESWAP_ASSET_ID])
+  const incentive = !loading
+    ? user.calculatedUserIncentives?.[reward.rewardTokenAddress]
+    : undefined
 
   const claimRewards = useRootStore((state) => state.claimRewards)
 
@@ -26,5 +31,6 @@ export const useClaimGdotReward = () => {
   return {
     action,
     reward,
+    incentive,
   }
 }
