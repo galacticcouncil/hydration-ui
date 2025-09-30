11f225d8d40cfe043e29545d217ad5b1376c6fe1
mrq1911 <2580779+mrq1911@users.noreply.github.com>
2025-09-30 18:07:33 +0200

    Revert "Rococo => prod"

--- src/sections/pools/stablepool/components/CurrencyReserves.tsx
+++ src/sections/pools/stablepool/components/CurrencyReserves.tsx
@@ -5,8 +5,6 @@ import { SRow } from "./CurrencyReserves.styled"
 import { useTranslation } from "react-i18next"
 import { DisplayValue } from "components/DisplayValue/DisplayValue"
 import { TStablepool } from "sections/pools/PoolsPage.utils"
-import BN from "bignumber.js"
-import { DollarAssetValue } from "components/DollarAssetValue/DollarAssetValue"
 
 type Props = {
   pool: TStablepool
@@ -21,7 +19,7 @@ export const CurrencyReserves = ({ pool }: Props) => {
       <Text fs={16} font="GeistMono" tTransform="uppercase" sx={{ mb: 5 }}>
         {t("liquidity.stablepool.reserves")}
       </Text>
-      {balances.map(({ id, symbol, percentage, balanceDisplay, balance }) => (
+      {balances.map(({ id, symbol, percentage, balanceDisplay }) => (
         <SRow key={id}>
           <div sx={{ flex: "row", align: "center", gap: 8 }}>
             <Icon size={24} icon={<AssetLogo id={id} />} />
@@ -29,28 +27,14 @@ export const CurrencyReserves = ({ pool }: Props) => {
               {symbol}
             </Text>
           </div>
-          <div sx={{ flex: "row", align: "flex-start", gap: 8 }}>
-            <div sx={{ flex: "column", align: "flex-end" }}>
-              <Text color="white" fs={14}>
-                {t("value", {
-                  value: balance,
-                  decimalPlaces:
-                    symbol?.includes("ETH") || symbol?.includes("BTC") ? 4 : 3,
-                })}
-              </Text>
-
-              <DollarAssetValue
-                value={BN(balanceDisplay)}
-                wrapper={(children) => (
-                  <Text fs={10} color="whiteish500">
-                    {children}
-                  </Text>
-                )}
-              >
-                <DisplayValue value={BN(balanceDisplay)} />
-              </DollarAssetValue>
-            </div>
-
+          <div sx={{ flex: "row", align: "center", gap: 8 }}>
+            <Text color="white" fs={14}>
+              {t("value", {
+                value: balanceDisplay,
+                decimalPlaces:
+                  symbol?.includes("ETH") || symbol?.includes("BTC") ? 4 : 3,
+              })}
+            </Text>
             <Text color="basic500" fs={14}>
               ({t("value.percentage", { value: percentage, decimalPlaces: 1 })})
             </Text>
