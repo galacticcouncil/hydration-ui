d52fd9fd2d58fc94f779e2b0d6e79f6e94d8ba37
Radoslav Karlík <radoslav.karlik@cleevio.com>
2025-12-09 09:37:19 +0100

    f/HYDRA-1247 - Show only relevant assets when creating xyk pool

--- apps/main/src/modules/liquidity/components/CreateIsolatedPool/CreateIsolatedPool.utils.ts
+++ apps/main/src/modules/liquidity/components/CreateIsolatedPool/CreateIsolatedPool.utils.ts
@@ -1,44 +1,55 @@
+import { standardSchemaResolver } from "@hookform/resolvers/standard-schema"
 import { useMutation } from "@tanstack/react-query"
 import Big from "big.js"
-import { t } from "i18next"
+import { useMemo } from "react"
+import { useForm } from "react-hook-form"
 import { useTranslation } from "react-i18next"
 import { z } from "zod/v4"
 
 import { TAssetData } from "@/api/assets"
+import { TAsset, useAssets } from "@/providers/assetsProvider"
 import { useRpcProvider } from "@/providers/rpcProvider"
+import { useAccountBalances } from "@/states/account"
 import { useTransactionsStore } from "@/states/transactions"
-import { positive, required, validateFieldMaxBalance } from "@/utils/validators"
+import {
+  positive,
+  required,
+  requiredObject,
+  useValidateFormMaxBalance,
+} from "@/utils/validators"
 
-export const zodCreateIsolatedPool = (
-  balanceA: string,
-  balanceB: string,
-  assetA?: TAssetData,
-  assetB?: TAssetData,
-) =>
-  z.object({
-    amountA: required
-      .pipe(positive)
-      .check(validateFieldMaxBalance(balanceA))
-      .refine(
-        () => {
-          return !!assetA
-        },
-        {
-          message: t("liquidity:liquidity.createPool.modal.validation.assetA"),
-        },
-      ),
-    amountB: required
-      .pipe(positive)
-      .check(validateFieldMaxBalance(balanceB))
-      .refine(
-        () => {
-          return !!assetB
-        },
-        {
-          message: t("liquidity:liquidity.createPool.modal.validation.assetB"),
-        },
-      ),
+const schema = z.object({
+  assetA: requiredObject<TAsset>(),
+  amountA: required.pipe(positive),
+  assetB: requiredObject<TAsset>(),
+  amountB: required.pipe(positive),
+})
+
+export type CreateIsolatedPoolFormData = z.infer<typeof schema>
+
+const useSchema = () => {
+  const refineFormMaxBalance = useValidateFormMaxBalance()
+
+  return schema.check(
+    refineFormMaxBalance("amountA", (form) => [form.assetA, form.amountA]),
+    refineFormMaxBalance("amountB", (form) => [form.assetB, form.amountB]),
+  )
+}
+
+const defaultValues: CreateIsolatedPoolFormData = {
+  assetA: null,
+  amountA: "",
+  assetB: null,
+  amountB: "",
+}
+
+export const useIsolatedPoolForm = () => {
+  return useForm<CreateIsolatedPoolFormData>({
+    mode: "onChange",
+    defaultValues,
+    resolver: standardSchemaResolver(useSchema()),
   })
+}
 
 export const useSubmitCreateIsolatedPool = ({
   onSubmitted,
@@ -79,4 +90,44 @@ export const useSubmitCreateIsolatedPool = ({
       await createTransaction({ tx, toasts }, { onSubmitted })
     },
   })
+}
+
+export const createPoolExclusivityMap = (
+  xykPoolsAssets: ReadonlyArray<readonly [a: string, idB: string]>,
+): Readonly<Record<string, ReadonlyArray<string>>> =>
+  xykPoolsAssets.reduce<Record<string, string[]>>((memo, [idA, idB]) => {
+    if (!memo[idA]) {
+      memo[idA] = []
+    }
+    if (!memo[idB]) {
+      memo[idB] = []
+    }
+    if (!memo[idA].includes(idB)) {
+      memo[idA].push(idB)
+    }
+    if (!memo[idB].includes(idA)) {
+      memo[idB].push(idA)
+    }
+    return memo
+  }, {})
+
+export const filterIdsByExclusivity = (
+  exclusiveId: string | undefined,
+  assets: TAsset[],
+  map: ReturnType<typeof createPoolExclusivityMap>,
+) =>
+  assets.filter(
+    (asset) =>
+      !exclusiveId ||
+      (asset.id !== exclusiveId && !map[exclusiveId]?.includes(asset.id)),
+  )
+
+export const useAllowedXYKPoolAssets = () => {
+  const { tradable } = useAssets()
+  const { getTransferableBalance } = useAccountBalances()
+
+  return useMemo(
+    () => tradable.filter((asset) => getTransferableBalance(asset.id) > 0n),
+    [tradable, getTransferableBalance],
+  )
 }
