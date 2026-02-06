57c1e44cfb32b9ff0b895b6008d7b6bdb1218afb
Radoslav Karlik <radoslav.karlik@cleevio.com>
2026-02-06 08:48:52 +0100

    f/HYDRA-1279 - General mobile fixes

--- apps/main/src/modules/layout/components/MobileTabBar/MobileTabBarAction.tsx
+++ apps/main/src/modules/layout/components/MobileTabBar/MobileTabBarAction.tsx
@@ -1,4 +1,4 @@
-import { Icon, Text } from "@galacticcouncil/ui/components"
+import { ExternalLink, Icon, Text } from "@galacticcouncil/ui/components"
 import { getToken } from "@galacticcouncil/ui/utils"
 import { FC, ReactNode } from "react"
 
@@ -7,16 +7,31 @@ import { STabBarMenuAction } from "@/modules/layout/components/MobileTabBar/Mobi
 type Props = {
   readonly icon: React.ComponentType
   readonly label: ReactNode
+  readonly link?: string
   readonly onClick?: () => void
 }
 
-export const MobileTabBarAction: FC<Props> = ({ icon, label, onClick }) => {
-  return (
+export const MobileTabBarAction: FC<Props> = ({
+  icon,
+  label,
+  link,
+  onClick,
+}) => {
+  const renderLink = (children: ReactNode) =>
+    link ? (
+      <ExternalLink sx={{ textDecoration: "none" }} href={link}>
+        {children}
+      </ExternalLink>
+    ) : (
+      children
+    )
+
+  return renderLink(
     <STabBarMenuAction onClick={onClick}>
       <Icon component={icon} />
       <Text fw={600} fs="p4" lh="m" color={getToken("text.high")}>
         {label}
       </Text>
-    </STabBarMenuAction>
+    </STabBarMenuAction>,
   )
 }
--- apps/main/src/modules/trade/orders/MyRecentActivity/MyRecentActivity.columns.tsx
+++ apps/main/src/modules/trade/orders/MyRecentActivity/MyRecentActivity.columns.tsx
@@ -1,3 +1,4 @@
+import { ClassNames } from "@emotion/react"
 import { DcaScheduleStatus } from "@galacticcouncil/indexer/squid"
 import { ArrowRightLeft, Trash } from "@galacticcouncil/ui/assets/icons"
 import {
@@ -13,7 +14,6 @@ import { createColumnHelper } from "@tanstack/react-table"
 import { useMemo, useState } from "react"
 import { useTranslation } from "react-i18next"
 
-import { AmountMobile } from "@/modules/trade/orders/columns/AmountMobile"
 import { DcaOrderStatus } from "@/modules/trade/orders/columns/DcaOrderStatus"
 import { SwapAmount } from "@/modules/trade/orders/columns/SwapAmount"
 import { SwapMobile } from "@/modules/trade/orders/columns/SwapMobile"
@@ -21,7 +21,6 @@ import { SwapPrice } from "@/modules/trade/orders/columns/SwapPrice"
 import { SwapStatus } from "@/modules/trade/orders/columns/SwapStatus"
 import { SwapType } from "@/modules/trade/orders/columns/SwapType"
 import { RoutedTradeData } from "@/modules/trade/orders/lib/useRoutedTradesData"
-import { SwapDetailsModal } from "@/modules/trade/orders/SwapDetailsModal"
 import { TerminateDcaScheduleModalContent } from "@/modules/trade/orders/TerminateDcaScheduleModalContent"
 
 const columnHelper = createColumnHelper<RoutedTradeData>()
@@ -149,37 +148,24 @@ export const useMyRecentActivityColumns = () => {
     const fromToColumnMobile = columnHelper.display({
       header: t("trade:trade.orders.fromTo.mobile"),
       cell: ({ row }) => {
-        return <SwapMobile from={row.original.from} to={row.original.to} />
-      },
-    })
-
-    const statusColumnMobile = columnHelper.display({
-      header: t("trade:trade.orders.status.mobile"),
-      meta: {
-        sx: { textAlign: "end" },
-      },
-      cell: function Cell({ row }) {
-        const [modal, setModal] = useState(false)
-
         return (
-          <>
-            <TableRowDetailsExpand onClick={() => setModal(true)}>
-              <AmountMobile
-                fromAmount={row.original.fromAmount}
-                from={row.original.from}
-                status={row.original.status?.status}
-              />
-            </TableRowDetailsExpand>
-            <Modal open={modal} onOpenChange={setModal}>
-              <SwapDetailsModal details={row.original} />
-            </Modal>
-          </>
+          <ClassNames>
+            {({ css }) => (
+              <TableRowDetailsExpand
+                containerClassName={css`
+                  justify-content: space-between;
+                `}
+              >
+                <SwapMobile from={row.original.from} to={row.original.to} />
+              </TableRowDetailsExpand>
+            )}
+          </ClassNames>
         )
       },
     })
 
     if (isMobile) {
-      return [fromToColumnMobile, statusColumnMobile]
+      return [fromToColumnMobile]
     }
 
     return [
