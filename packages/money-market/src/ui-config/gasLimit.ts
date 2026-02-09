19ff2c6358e46c85ffff97052379ece05c8b6103
Jakub Vonášek <vonasek.jakub@gmail.com>
2026-02-09 18:06:39 +0100

    Bump withdraw gas limit

--- packages/money-market/src/ui-config/gasLimit.ts
+++ packages/money-market/src/ui-config/gasLimit.ts
@@ -7,16 +7,16 @@ import {
 export const gasLimitRecommendations: GasRecommendationType = {
   ...gasLimits,
   [ProtocolAction.withdraw]: {
-    limit: "600000",
-    recommended: "600000",
+    limit: "1000000",
+    recommended: "1000000",
   },
   [ProtocolAction.borrow]: {
     limit: "1000000",
     recommended: "1000000",
   },
   [ProtocolAction.setUsageAsCollateral]: {
-    limit: "800000",
-    recommended: "800000",
+    limit: "1000000",
+    recommended: "1000000",
   },
   [ProtocolAction.claimRewards]: {
     limit: "1000000",
