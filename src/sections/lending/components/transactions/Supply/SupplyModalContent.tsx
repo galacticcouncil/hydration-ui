0adcea54a6f014d1c7db678bcccd86c88cd325c2
vkulinich <viacheslav.kulinich@cleevio.com>
2026-02-12 13:32:04 +0100

    debug

--- src/sections/lending/components/transactions/Supply/SupplyModalContent.tsx
+++ src/sections/lending/components/transactions/Supply/SupplyModalContent.tsx
@@ -196,7 +196,12 @@ export const SupplyModalContent = React.memo(
       (userReserve && userReserve.underlyingBalance !== "0"
         ? userReserve.usageAsCollateralEnabledOnUser
         : true)
-
+    console.log({
+      isInIsolationMode: user.isInIsolationMode,
+      isIsolated: poolReserve.isIsolated,
+      hasDifferentCollateral,
+      userReserve,
+    })
     // token info to add to wallet
     const addToken: ERC20TokenType = {
       address: poolReserve.aTokenAddress,
