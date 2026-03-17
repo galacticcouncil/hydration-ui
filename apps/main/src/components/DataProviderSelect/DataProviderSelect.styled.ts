70830d6e854ea4b1ef05dc955a6c8253d90f6a22
Jakub Vonášek <vonasek.jakub@gmail.com>
2026-03-17 16:31:42 +0100

    Classic UI Button

--- apps/main/src/components/DataProviderSelect/DataProviderSelect.styled.ts
+++ apps/main/src/components/DataProviderSelect/DataProviderSelect.styled.ts
@@ -1,29 +1,33 @@
-import { css } from "@emotion/react"
+import { css, Theme } from "@emotion/react"
 import styled from "@emotion/styled"
 import { mq } from "@galacticcouncil/ui/theme"
+import { pxToRem } from "@galacticcouncil/ui/utils"
 
-const bottomPinnedStyle = css`
+const bottomPinnedStyle = (theme: Theme) => css`
   position: fixed;
   right: 0;
   bottom: 0;
-  padding-bottom: 8px;
+  padding-bottom: ${theme.space.base};
   z-index: 1001;
 `
 
 export const SContainer = styled.div<{ readonly bottomPinned?: boolean }>(
-  ({ bottomPinned }) => css`
-    padding: 8px;
-    padding-bottom: 80px;
+  ({ theme, bottomPinned }) => css`
+    display: flex;
+
+    gap: ${theme.space.base};
+    padding: ${theme.space.base};
+    padding-bottom: ${pxToRem(80)};
 
     ${bottomPinned
-      ? bottomPinnedStyle
+      ? bottomPinnedStyle(theme)
       : css`
           display: flex;
           justify-content: end;
         `}
 
     ${mq("lg")} {
-      ${bottomPinnedStyle}
+      ${bottomPinnedStyle(theme)}
     }
   `,
 )
