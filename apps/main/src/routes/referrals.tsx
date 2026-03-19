7717852b14b6dabb57a3183e4c5797eca8cbea88
vkulinich <viacheslav.kulinich@cleevio.com>
2026-03-19 19:49:18 +0100

    Fix Referral code link

--- apps/main/src/routes/referrals.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router"
-
-export const Route = createFileRoute("/referrals")({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>N/A</div>
-}
