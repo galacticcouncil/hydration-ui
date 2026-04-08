256d1baafb78fda7d4a7b8835a1be44fee4d4ae7
Jakub Vonášek <vonasek.jakub@gmail.com>
2026-04-08 12:48:21 +0200

    Reown WalletConnect

--- packages/web3-connect/src/config/wallet.ts
+++ packages/web3-connect/src/config/wallet.ts
@@ -1,5 +1,7 @@
 export const WALLET_DAPP_NAME = "Hydration"
 
+export const REOWN_PROJECT_ID = "265a3fea03b46c14a46a201fbd6c552e"
+
 export enum WalletProviderStatus {
   Connected = "connected",
   Pending = "pending",
