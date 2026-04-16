d9d8c99c5750cb1161224403af0f988441900446
vkulinich <viacheslav.kulinich@cleevio.com>
2026-04-16 10:54:37 +0200

    undo change

--- apps/main/src/components/TabMenu/TabMenuItem.tsx
+++ apps/main/src/components/TabMenu/TabMenuItem.tsx
@@ -61,6 +61,7 @@ export const TabMenuItem: FC<Props> = ({
       variant={isActive(to, search) ? activeVariant : variant}
       size={size}
       asChild
+      sx={{ minWidth: "2xl" }}
     >
       <Link
         to={to}
