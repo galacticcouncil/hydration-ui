a6ccc3ec95ad23d36cff6dde02eff26a182289a0
Jakub Vonášek <vonasek.jakub@gmail.com>
2026-04-16 17:03:20 +0200

    Enable deposit withdraw

--- apps/main/src/api/external/pendulum.ts
+++ apps/main/src/api/external/pendulum.ts
@@ -1,4 +1,4 @@
 import { chainsMap } from "@galacticcouncil/xc-cfg"
-import { Parachain } from "@galacticcouncil/xc-core/build/types/chain"
+import { Parachain } from "@galacticcouncil/xc-core"
 
 export const pendulum = chainsMap.get("pendulum") as Parachain
