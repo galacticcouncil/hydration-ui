bc817ce225f7a8661ef89515229ca860f57472b4
Pavol Noha <pavol.noha@gmail.com>
2026-05-30 10:50:48 -0600

    remove classic ui link

--- apps/main/src/components/DataProviderSelect/DataProviderSelect.tsx
+++ apps/main/src/components/DataProviderSelect/DataProviderSelect.tsx
@@ -1,27 +1,21 @@
 import {
   Button,
-  ExternalLink,
   Skeleton,
   Spinner,
-  Text,
   Tooltip,
 } from "@galacticcouncil/ui/components"
 import { useBreakpoints } from "@galacticcouncil/ui/theme"
-import { getToken } from "@galacticcouncil/ui/utils"
 import { useQuery } from "@tanstack/react-query"
 import { useState } from "react"
-import { useTranslation } from "react-i18next"
 
 import { bestNumberQuery } from "@/api/chain"
 import { useActiveProviderProps, useSquidUrl } from "@/api/provider"
 import { RpcStatus } from "@/components/DataProviderSelect/components/rpc/RpcStatus"
 import { StatusTooltipContent } from "@/components/DataProviderSelect/components/StatusTooltipContent"
 import { DataProviderSelectModal } from "@/components/DataProviderSelect/DataProviderSelectModal"
-import { CLASSIC_UI_LINK } from "@/config/links"
 import { useRpcProvider } from "@/providers/rpcProvider"
 
 export const DataProviderSelect = () => {
-  const { t } = useTranslation(["common"])
   const [modalOpen, setModalOpen] = useState(false)
   const provider = useRpcProvider()
   const { isMobile } = useBreakpoints()
@@ -32,13 +26,6 @@ export const DataProviderSelect = () => {
 
   return (
     <>
-      <Button variant="tertiary" outline size="small" blur asChild>
-        <ExternalLink href={CLASSIC_UI_LINK}>
-          <Text fw={600} as="span" color={getToken("text.high")}>
-            {t("classicUi")}
-          </Text>
-        </ExternalLink>
-      </Button>
       <Tooltip
         text={
           !isMobile &&
