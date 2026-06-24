cf2be1c7e2bf61a7938e6965e5e0db04f38d30b5
Jakub Vonášek <vonasek.jakub@gmail.com>
2026-06-24 15:26:56 +0200

    Filter out xc swap journeys

--- apps/main/src/config/xcSwap.ts
+++ apps/main/src/config/xcSwap.ts
@@ -34,6 +34,7 @@ export const getXcSwapAssetLogoUrl = (assetKey: string): string => {
 
 export const XC_SWAP_CONFIG = {
   emitter: "0x059ed5658c988976e73adb6597418970414f3dd0",
+  receiver: "0xf1a5fe4252d9a1c39b0fb9de1f19049ee57ed188",
   defaults: {
     source: {
       chainKey: HYDRATION_CHAIN_KEY,
