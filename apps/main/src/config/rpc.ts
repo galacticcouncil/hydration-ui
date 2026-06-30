15cd9da89b5ffabc171588f939efe6cc4a190493
Jakub Vonášek <vonasek.jakub@gmail.com>
2026-06-30 18:44:17 +0200

    Remove rpcs

--- apps/main/src/config/rpc.ts
+++ apps/main/src/config/rpc.ts
@@ -63,10 +63,10 @@ export const PROVIDERS: ProviderProps[] = [
   //createProvider("parm", "wss://rpc.parm.hydration.cloud"),
   createProvider("sin", "wss://subway.sin.hydration.cloud"),
   createProvider("coke", "wss://subway.coke.hydration.cloud"),
-  createProvider("kril", "wss://rpc.kril.hydration.cloud"),
+  // createProvider("kril", "wss://rpc.kril.hydration.cloud"),
   createProvider("shellfish", "wss://subway.shellfish.hydration.cloud"),
   createProvider("catfish-1", "wss://rpc-catfish-1.catfish.hydration.cloud"),
-  createProvider("catfish-2", "wss://rpc-catfish-2.catfish.hydration.cloud"),
+  // createProvider("catfish-2", "wss://rpc-catfish-2.catfish.hydration.cloud"),
   createProvider("catfish-3", "wss://rpc-catfish-3.catfish.hydration.cloud"),
   createProvider("catfish-4", "wss://rpc-catfish-4.catfish.hydration.cloud"),
   createProvider(
