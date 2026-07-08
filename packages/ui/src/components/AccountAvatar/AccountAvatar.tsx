87d528b3f122a93fe47eb8aa48db070ad2619021
Jakub Vonášek <vonasek.jakub@gmail.com>
2026-07-09 01:00:51 +0200

    Identican

--- packages/ui/src/components/AccountAvatar/AccountAvatar.tsx
+++ packages/ui/src/components/AccountAvatar/AccountAvatar.tsx
@@ -1,52 +1,22 @@
-import { EvmAddr, SolanaAddr, Ss58Addr, SuiAddr } from "@galacticcouncil/utils"
-import { lazy, Suspense } from "react"
+import { Suspense } from "react"
 
 import { EmptyIdenticon } from "@/components/AccountAvatar/identicons/EmptyIdenticon"
-import { SolanaIdenticon } from "@/components/AccountAvatar/identicons/SolanaIdenticon"
-import { SuiIdenticon } from "@/components/AccountAvatar/identicons/SuiIdenticon"
+import { Identican } from "@/components/AccountAvatar/identicons/Identican"
 import { Box, BoxProps } from "@/components/Box"
 import { useUiScale } from "@/styles/media"
 import { getToken } from "@/utils"
 
-const PolkadotIdenticon = lazy(async () => ({
-  default: await import(
-    "@/components/AccountAvatar/identicons/PolkadotIdenticon"
-  ).then((m) => m.PolkadotIdenticon),
-}))
-
-const TalismanIdenticon = lazy(async () => ({
-  default: await import(
-    "@/components/AccountAvatar/identicons/TalismanIdenticon"
-  ).then((m) => m.TalismanIdenticon),
-}))
-
-const EthereumIdenticon = lazy(async () => ({
-  default: await import(
-    "@/components/AccountAvatar/identicons/EthereumIdenticon"
-  ).then((m) => m.EthereumIdenticon),
-}))
-
-export type AccountAvatarTheme =
-  | "auto"
-  | "polkadot"
-  | "evm"
-  | "talisman"
-  | "solana"
-  | "sui"
 export type AccountAvatarProps = BoxProps & {
   address: string
   size?: number
-  theme?: AccountAvatarTheme
 }
 
 export const AccountAvatar: React.FC<AccountAvatarProps> = ({
-  size = 32,
-  theme = "auto",
+  size = 42,
   ...props
 }) => {
   const uiScale = useUiScale()
   const scaledSize = size * uiScale
-  const chosenTheme = theme === "auto" ? getAutoTheme(props.address) : theme
 
   return (
     <Suspense
@@ -58,35 +28,11 @@ export const AccountAvatar: React.FC<AccountAvatarProps> = ({
         />
       }
     >
-      {chosenTheme === null && <EmptyIdenticon size={scaledSize} />}
-      {chosenTheme === "evm" && (
-        <EthereumIdenticon size={scaledSize} {...props} />
+      {!props.address ? (
+        <EmptyIdenticon size={scaledSize} {...props} />
+      ) : (
+        <Identican size={scaledSize} {...props} />
       )}
-      {chosenTheme === "talisman" && (
-        <TalismanIdenticon size={scaledSize} {...props} />
-      )}
-      {chosenTheme === "polkadot" && (
-        <PolkadotIdenticon size={scaledSize} {...props} />
-      )}
-      {chosenTheme === "solana" && (
-        <SolanaIdenticon size={scaledSize} {...props} />
-      )}
-      {chosenTheme === "sui" && <SuiIdenticon size={scaledSize} {...props} />}
     </Suspense>
   )
-}
-
-function getAutoTheme(address: string): AccountAvatarTheme | null {
-  switch (true) {
-    case EvmAddr.isValid(address):
-      return "evm"
-    case Ss58Addr.isValid(address):
-      return "polkadot"
-    case SolanaAddr.isValid(address):
-      return "solana"
-    case SuiAddr.isValid(address):
-      return "sui"
-    default:
-      return null
-  }
 }
