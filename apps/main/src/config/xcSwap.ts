ee05b7004f67c647eb7017b48b59d18aa294b708
Pavol Noha <pavol.noha@gmail.com>
2026-08-28 15:11:46 +0200

    near: fix configs + bump deps

--- apps/main/src/config/xcSwap.ts
+++ apps/main/src/config/xcSwap.ts
@@ -3,8 +3,8 @@ import { HOLLAR_ASSET_ID, HYDRATION_CHAIN_KEY } from "@galacticcouncil/utils"
 import { NATIVE_ASSET_ID } from "@/utils/consts"
 
 export const XC_SWAP_CONFIG = {
-  emitter: "0x059ed5658c988976e73adb6597418970414f3dd0",
-  receiver: "0xf1a5fe4252d9a1c39b0fb9de1f19049ee57ed188",
+  emitter: "0x98f1ebc9dcc8ab7ba54d83c98500e9e313f793f2",
+  receiver: "0x2173f6ece25768e7efc5199f70f8783d88ba63c8",
   defaults: {
     source: {
       chainKey: HYDRATION_CHAIN_KEY,
